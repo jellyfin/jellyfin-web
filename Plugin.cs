e389d80caccc59efbc4875083c6c6e4245944065
LukePulverenti Luke Pulverenti luke pulverenti <LukePulverenti Luke Pulverenti luke.pulverenti@gmail.com>
2012-08-17 15:07:21 -0400

    Removed more kernel methods and deprecated the Configuration and HtmlBrowser projects. They've both been replaced by the new WebDashboard project.

---
+++ Plugin.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.Composition;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Plugins;
+
+namespace MediaBrowser.WebDashboard
+{
+    [Export(typeof(BasePlugin))]
+    public class Plugin : BaseGenericPlugin<BasePluginConfiguration>
+    {
+        public override string Name
+        {
+            get { return "Dashboard"; }
+        }
+    }
+}
