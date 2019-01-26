5428c37200ef8428e8b02fb795fed8706f1d0961
Andrew Rabert <ar@nullsum.net>
2019-01-25 23:54:54 -0500

    Fix user edit save Remove connect logic

--- src/scripts/useredit.js
+++ src/scripts/useredit.js
@@ -32,13 +32,10 @@ define(["jQuery", "loading", "libraryMenu", "fnchecked"], function($, loading, l
     }
 
     function onSaveComplete(page, user) {
-        loading.hide(), (currentUser.ConnectUserName || "") == $("#txtConnectUserName", page).val() ? require(["toast"], function(toast) {
-            toast(Globalize.translate("SettingsSaved"))
-        }) : require(["connectHelper"], function(connectHelper) {
-            connectHelper.updateUserLink(ApiClient, user, $("#txtConnectUserName", page).val()).then(function() {
-                loadData(page)
-            })
-        })
+        loading.hide();
+        require(["toast"], function(toast) {
+            toast(Globalize.translate("SettingsSaved"));
+        });
     }
 
     function saveUser(user, page) {
