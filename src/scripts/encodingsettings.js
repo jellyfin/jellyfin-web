36a0df3c7d4398988378d961fb046fafc1a7a247
Claus Vium <cvium@users.noreply.github.com>
2019-03-08 09:15:19 +0000

    Update src/scripts/encodingsettings.js
    Co-Authored-By: ploughpuff <33969763+ploughpuff@users.noreply.github.com>

--- src/scripts/encodingsettings.js
+++ src/scripts/encodingsettings.js
@@ -16,7 +16,7 @@ define(["jQuery", "loading", "globalize", "dom"], function($, loading, globalize
         page.querySelector("#txtH264Crf").value = config.H264Crf || "";
         page.querySelector("#chkEnableSubtitleExtraction").checked = config.EnableSubtitleExtraction || false;
         page.querySelector("#selectVideoDecoder").dispatchEvent(new CustomEvent("change", {
-            bubbles: !0
+             bubbles: true
         })), loading.hide()
     }
 
