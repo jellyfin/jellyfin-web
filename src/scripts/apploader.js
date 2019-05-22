ac98d7100f7638e14a4d38109479e27ec629b746
Bill Thornton <billt2006@gmail.com>
2019-05-22 14:49:57 -0400

    Deuglify apploader.js

--- src/scripts/apploader.js
+++ src/scripts/apploader.js
@@ -1,12 +1,24 @@
-! function() {
+(function() {
     "use strict";
 
     function loadApp() {
         var script = document.createElement("script"),
             src = "./scripts/site.js";
-        self.dashboardVersion && (src += "?v=" + self.dashboardVersion), script.src = src, document.head.appendChild(script)
-    }! function() {
+        if (self.dashboardVersion) {
+            src += "?v=" + self.dashboardVersion;
+        }
+        script.src = src;
+        document.head.appendChild(script);
+    }
+
+    (function() {
         var src, script = document.createElement("script");
-        src = self.Promise ? "./bower_components/alameda/alameda.js" : "./bower_components/requirejs/require.js", self.dashboardVersion && (src += "?v=" + self.dashboardVersion), script.src = src, script.onload = loadApp, document.head.appendChild(script)
-    }()
-}();
+        src = self.Promise ? "./bower_components/alameda/alameda.js" : "./bower_components/requirejs/require.js";
+        if (self.dashboardVersion) {
+            src += "?v=" + self.dashboardVersion;
+        }
+        script.src = src;
+        script.onload = loadApp;
+        document.head.appendChild(script);
+    })();
+})();
