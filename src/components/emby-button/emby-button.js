76d5f8474c095017a913a9ac9d41cb71d80c9c47
grafixeyehero <grafixeyehero>
2019-06-10 20:44:13 +0300

    Remove class button-link-inline and css style

--- src/components/emby-button/emby-button.js
+++ src/components/emby-button/emby-button.js
@@ -28,8 +28,6 @@ define(['browser', 'dom', 'layoutManager', 'shell', 'appRouter', 'apphost', 'css
 
         this.classList.add('emby-button');
 
-        this.classList.add('button-link-inline');
-
         if (layoutManager.tv) {
             if (this.getAttribute('data-focusscale') !== 'false') {
                 this.classList.add('emby-button-focusscale');
