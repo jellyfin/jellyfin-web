03767e85fab02443052a781c443283f023e86925
grafixeyehero <grafixeyehero>
2019-07-28 15:50:39 +0300

    apploader

--- src/scripts/apploader.js
+++ src/scripts/apploader.js
@@ -1,12 +1,45 @@
-! function() {
+(function() {
+
     "use strict";
 
+    function loadRequire() {
+
+        var script = document.createElement('script');
+
+        var src;
+
+        if (self.Promise) {
+            src = './bower_components/alameda/alameda.js';
+        } else {
+            src = './bower_components/requirejs/require.js';
+        }
+
+        if (self.dashboardVersion) {
+            src += '?v=' + self.dashboardVersion;
+        }
+
+        script.src = src;
+
+        script.onload = loadApp;
+
+        document.head.appendChild(script);
+    }
+
     function loadApp() {
-        var script = document.createElement("script"),
-            src = "./scripts/site.js";
-        self.dashboardVersion && (src += "?v=" + self.dashboardVersion), script.src = src, document.head.appendChild(script)
-    }! function() {
-        var src, script = document.createElement("script");
-        src = self.Promise ? "./bower_components/alameda/alameda.js" : "./bower_components/requirejs/require.js", self.dashboardVersion && (src += "?v=" + self.dashboardVersion), script.src = src, script.onload = loadApp, document.head.appendChild(script)
-    }()
-}();
+
+        var script = document.createElement('script');
+
+        var src = './scripts/site.js';
+
+        if (self.dashboardVersion) {
+            src += '?v=' + self.dashboardVersion;
+        }
+
+        script.src = src;
+
+        document.head.appendChild(script);
+    }
+
+    loadRequire();
+
+})();
