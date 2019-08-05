d441dd9702dd304ec7cb821527209b16ee8b0044
grafixeyehero <grafixeyehero>
2019-08-05 04:41:22 +0300

    tvupcoming

--- src/scripts/tvupcoming.js
+++ src/scripts/tvupcoming.js
@@ -2,23 +2,41 @@ define(["layoutManager", "loading", "datetime", "libraryBrowser", "cardBuilder",
     "use strict";
 
     function getUpcomingPromise(context, params) {
+
         loading.show();
+
         var query = {
+
             Limit: 48,
             Fields: "AirTime,UserData",
             UserId: ApiClient.getCurrentUserId(),
             ImageTypeLimit: 1,
             EnableImageTypes: "Primary,Backdrop,Banner,Thumb",
-            EnableTotalRecordCount: !1
+            EnableTotalRecordCount: false
         };
-        return query.ParentId = params.topParentId, ApiClient.getJSON(ApiClient.getUrl("Shows/Upcoming", query))
+
+        query.ParentId = params.topParentId;
+
+        return ApiClient.getJSON(ApiClient.getUrl("Shows/Upcoming", query));
     }
 
     function loadUpcoming(context, params, promise) {
-        promise.then(function(result) {
+
+        promise.then(function (result) {
+
             var items = result.Items;
-            items.length ? context.querySelector(".noItemsMessage").style.display = "none" : context.querySelector(".noItemsMessage").style.display = "block", renderUpcoming(context.querySelector("#upcomingItems"), items), loading.hide()
-        })
+
+            if (items.length) {
+                context.querySelector('.noItemsMessage').style.display = 'none';
+            } else {
+                context.querySelector('.noItemsMessage').style.display = 'block';
+            }
+
+            var elem = context.querySelector('#upcomingItems');
+            renderUpcoming(elem, items);
+
+            loading.hide();
+        });
     }
 
     function enableScrollX() {
@@ -30,61 +48,114 @@ define(["layoutManager", "loading", "datetime", "libraryBrowser", "cardBuilder",
     }
 
     function renderUpcoming(elem, items) {
-        var i, length, groups = [],
-            currentGroupName = "",
-            currentGroup = [];
+
+        var groups = [];
+
+        var currentGroupName = '';
+        var currentGroup = [];
+
+        var i, length;
+
         for (i = 0, length = items.length; i < length; i++) {
-            var item = items[i],
-                dateText = "";
-            if (item.PremiereDate) try {
-                var premiereDate = datetime.parseISO8601Date(item.PremiereDate, !0);
-                dateText = datetime.isRelativeDay(premiereDate, -1) ? Globalize.translate("Yesterday") : datetime.toLocaleDateString(premiereDate, {
-                    weekday: "long",
-                    month: "short",
-                    day: "numeric"
-                })
-            } catch (err) {}
-            dateText != currentGroupName ? (currentGroup.length && groups.push({
-                name: currentGroupName,
-                items: currentGroup
-            }), currentGroupName = dateText, currentGroup = [item]) : currentGroup.push(item)
+
+            var item = items[i];
+
+            var dateText = '';
+
+            if (item.PremiereDate) {
+                try {
+
+                    var premiereDate = datetime.parseISO8601Date(item.PremiereDate, true);
+
+                    if (datetime.isRelativeDay(premiereDate, -1)) {
+                        dateText = Globalize.translate('Yesterday');
+                    } else {
+                        dateText = datetime.toLocaleDateString(premiereDate, {
+                            weekday: 'long',
+                            month: 'short',
+                            day: 'numeric'
+                        });
+                    }
+
+                } catch (err) {
+                }
+            }
+
+            if (dateText != currentGroupName) {
+
+                if (currentGroup.length) {
+                    groups.push({
+                        name: currentGroupName,
+                        items: currentGroup
+                    });
+                }
+
+                currentGroupName = dateText;
+                currentGroup = [item];
+            } else {
+                currentGroup.push(item);
+            }
         }
-        var html = "";
+
+        var html = '';
+
         for (i = 0, length = groups.length; i < length; i++) {
+
             var group = groups[i];
-            html += '<div class="verticalSection">', html += '<h2 class="sectionTitle sectionTitle-cards padded-left">' + group.name + "</h2>";
-            var allowBottomPadding = !0;
+
+            html += '<div class="verticalSection">';
+            html += '<h2 class="sectionTitle sectionTitle-cards padded-left">' + group.name + '</h2>';
+
+            var allowBottomPadding = true;
+
             if (enableScrollX()) {
-                allowBottomPadding = !1;
-                var scrollXClass = "scrollX hiddenScrollX";
-                layoutManager.tv && (scrollXClass += " smoothScrollX"), html += '<div is="emby-itemscontainer" class="itemsContainer ' + scrollXClass + ' padded-left padded-right">'
-            } else html += '<div is="emby-itemscontainer" class="itemsContainer vertical-wrap padded-left padded-right">';
-            var supportsImageAnalysis = appHost.supports("imageanalysis");
-            supportsImageAnalysis = !1, html += cardBuilder.getCardsHtml({
+                allowBottomPadding = false;
+                html += '<div is="emby-itemscontainer" class="itemsContainer hiddenScrollX padded-left padded-right">';
+            } else {
+                html += '<div is="emby-itemscontainer" class="itemsContainer vertical-wrap padded-left padded-right">';
+            }
+
+            var supportsImageAnalysis = appHost.supports('imageanalysis');
+            supportsImageAnalysis = false;
+
+            html += cardBuilder.getCardsHtml({
                 items: group.items,
-                showLocationTypeIndicator: !1,
+                showLocationTypeIndicator: false,
                 shape: getThumbShape(),
-                showTitle: !0,
-                preferThumb: !0,
-                lazy: !0,
-                showDetailsMenu: !0,
+                showTitle: true,
+                preferThumb: true,
+                lazy: true,
+                showDetailsMenu: true,
                 centerText: !supportsImageAnalysis,
-                showParentTitle: !0,
-                overlayText: !1,
+                showParentTitle: true,
+                overlayText: false,
                 allowBottomPadding: allowBottomPadding,
                 cardLayout: supportsImageAnalysis,
-                overlayMoreButton: !0,
-                missingIndicator: !1
-            }), html += "</div>", html += "</div>"
+                overlayMoreButton: true,
+                missingIndicator: false
+
+            });
+            html += '</div>';
+
+            html += '</div>';
         }
-        elem.innerHTML = html, imageLoader.lazyChildren(elem)
+
+        elem.innerHTML = html;
+        imageLoader.lazyChildren(elem);
     }
-    return function(view, params, tabContent) {
-        var upcomingPromise, self = this;
-        self.preRender = function() {
-            upcomingPromise = getUpcomingPromise(view, params)
-        }, self.renderTab = function() {
-            loadUpcoming(tabContent, params, upcomingPromise)
-        }
-    }
+
+    return function (view, params, tabContent) {
+
+        var self = this;
+        var upcomingPromise;
+
+        self.preRender = function () {
+            upcomingPromise = getUpcomingPromise(view, params);
+        };
+
+        self.renderTab = function () {
+
+            loadUpcoming(tabContent, params, upcomingPromise);
+        };
+    };
 });
