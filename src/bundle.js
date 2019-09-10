3db0267e55386ff0d72ca5d5b5bc870f85be20ef
dkanada <dkanada@users.noreply.github.com>
2019-09-09 18:31:06 -0700

    hlsjs

--- src/bundle.js
+++ src/bundle.js
@@ -5,5 +5,14 @@
 var _define = window.define;
 
 var jstree = require("jstree");
+var hlsjs = require("hls.js");
+
 require("jstree/dist/themes/default/style.css");
+
 _define("jstree", ["jQuery"], function() { return jstree; });
+
+// 0.10.0 is the version used in the source
+// the newer version doesnt look like it should cause issues
+// thought we might need to add the dependencies here as well
+//_define("hlsjs", ["eventemitter3", "url-toolkit"], function() { return hlsjs; });
+_define("hlsjs", function() { return hlsjs; });
