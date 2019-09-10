36ca83923ded6ecc3fb5cbb80ea3fd249b51539e
Bill Thornton <billt2006@gmail.com>
2019-09-10 11:28:59 -0400

    Fix webpack libraryTarget config

--- src/bundle.js
+++ src/bundle.js
@@ -4,15 +4,11 @@
 // Use define from require.js not webpack's define
 var _define = window.define;
 
+// jstree
 var jstree = require("jstree");
-var hlsjs = require("hls.js");
-
 require("jstree/dist/themes/default/style.css");
-
 _define("jstree", ["jQuery"], function() { return jstree; });
 
-// 0.10.0 is the version used in the source
-// the newer version doesnt look like it should cause issues
-// thought we might need to add the dependencies here as well
-//_define("hlsjs", ["eventemitter3", "url-toolkit"], function() { return hlsjs; });
+// hlsjs
+var hlsjs = require("hls.js");
 _define("hlsjs", function() { return hlsjs; });
