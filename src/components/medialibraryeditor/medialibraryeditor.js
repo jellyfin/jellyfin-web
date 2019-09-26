6c566847db158be41e2559261d289aa1191f057f
Bill Thornton <billt2006@gmail.com>
2019-09-26 17:23:49 -0400

    Fix missing jQuery in medialibraryeditor

--- src/components/medialibraryeditor/medialibraryeditor.js
+++ src/components/medialibraryeditor/medialibraryeditor.js
@@ -1,4 +1,4 @@
-define(["loading", "dialogHelper", "dom", "components/libraryoptionseditor/libraryoptionseditor", "emby-button", "listViewStyle", "paper-icon-button-light", "formDialogStyle", "emby-toggle", "flexStyles"], function(loading, dialogHelper, dom, libraryoptionseditor) {
+define(["jQuery", "loading", "dialogHelper", "dom", "components/libraryoptionseditor/libraryoptionseditor", "emby-button", "listViewStyle", "paper-icon-button-light", "formDialogStyle", "emby-toggle", "flexStyles"], function(jQuery, loading, dialogHelper, dom, libraryoptionseditor) {
     "use strict";
 
     function onEditLibrary() {
