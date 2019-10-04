f7d1c9fb1bffb78a05392fb17ee406188941f5a8
Anthony Lavado <anthony@lavado.ca>
2019-10-04 03:41:00 -0400

    Resolves missing policy error on user creation
    Restores the previous method of posting the Users/New request, but adds the password field as well.

--- src/bower_components/apiclient/apiclientcore.js
+++ src/bower_components/apiclient/apiclientcore.js
@@ -1195,8 +1195,12 @@ define(["events", "appStorage"], function(events, appStorage) {
         return this.ajax({
             type: "POST",
             url: url,
-            data: JSON.stringify(user),
-            contentType: "application/json"
+            data: {
+                    Name: user.Name,
+                    Password: user.Password
+
+                },
+            dataType: "json"
         })
     }, ApiClient.prototype.updateUser = function(user) {
         if (!user) throw new Error("null user");
