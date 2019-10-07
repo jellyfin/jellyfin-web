5ebf1bda80f296a572e73d0edac1a9f1fcca1d2a
grafixeyehero <grafixeyehero>
2019-10-08 01:29:47 +0300

    themeloader.js

--- src/scripts/themeloader.js
+++ src/scripts/themeloader.js
@@ -1,15 +1,27 @@
-define(["userSettings", "skinManager", "connectionManager", "events"], function(userSettings, skinManager, connectionManager, events) {
+define(["userSettings", "skinManager", "connectionManager", "events"], function (userSettings, skinManager, connectionManager, events) {
     "use strict";
+
     var currentViewType;
-    pageClassOn("viewbeforeshow", "page", function() {
-        var classList = this.classList,
-            viewType = classList.contains("type-interior") || classList.contains("wizardPage") ? "a" : "b";
+    pageClassOn("viewbeforeshow", "page", function () {
+        var classList = this.classList;
+        var viewType = classList.contains("type-interior") || classList.contains("wizardPage") ? "a" : "b";
+
         if (viewType !== currentViewType) {
             currentViewType = viewType;
-            var theme, context;
-            "a" === viewType ? (theme = userSettings.dashboardTheme(), context = "serverdashboard") : theme = userSettings.theme(), skinManager.setTheme(theme, context)
+            var theme;
+            var context;
+
+            if ("a" === viewType) {
+                theme = userSettings.dashboardTheme();
+                context = "serverdashboard";
+            } else {
+                theme = userSettings.theme();
+            }
+
+            skinManager.setTheme(theme, context);
         }
-    }), events.on(connectionManager, "localusersignedin", function(e, user) {
-        currentViewType = null
-    })
+    });
+    events.on(connectionManager, "localusersignedin", function (e, user) {
+        currentViewType = null;
+    });
 });
