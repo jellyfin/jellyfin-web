e45798e2dd816ce21c36f5f57dd54b301f104fa2
grafixeyehero <grafixeyehero>
2019-10-09 02:51:39 +0300

    device

--- src/controllers/device.js
+++ src/controllers/device.js
@@ -1,23 +1,25 @@
-define(["loading", "libraryMenu", "dom", "emby-input", "emby-button"], function(loading, libraryMenu, dom) {
+define(["loading", "libraryMenu", "dom", "emby-input", "emby-button"], function (loading, libraryMenu, dom) {
     "use strict";
 
     function load(page, device, deviceOptions) {
-        page.querySelector("#txtCustomName", page).value = deviceOptions.CustomName || "", page.querySelector(".reportedName", page).innerHTML = device.Name || ""
+        page.querySelector("#txtCustomName", page).value = deviceOptions.CustomName || "";
+        page.querySelector(".reportedName", page).innerHTML = device.Name || "";
     }
 
     function loadData() {
         var page = this;
         loading.show();
-        var id = getParameterByName("id"),
-            promise1 = ApiClient.getJSON(ApiClient.getUrl("Devices/Info", {
-                Id: id
-            })),
-            promise2 = ApiClient.getJSON(ApiClient.getUrl("Devices/Options", {
-                Id: id
-            }));
-        Promise.all([promise1, promise2]).then(function(responses) {
-            load(page, responses[0], responses[1]), loading.hide()
-        })
+        var id = getParameterByName("id");
+        var promise1 = ApiClient.getJSON(ApiClient.getUrl("Devices/Info", {
+            Id: id
+        }));
+        var promise2 = ApiClient.getJSON(ApiClient.getUrl("Devices/Options", {
+            Id: id
+        }));
+        Promise.all([promise1, promise2]).then(function (responses) {
+            load(page, responses[0], responses[1]);
+            loading.hide();
+        });
     }
 
     function save(page) {
@@ -31,14 +33,18 @@ define(["loading", "libraryMenu", "dom", "emby-input", "emby-button"], function(
                 CustomName: page.querySelector("#txtCustomName").value
             }),
             contentType: "application/json"
-        }).then(Dashboard.processServerConfigurationUpdateResult)
+        }).then(Dashboard.processServerConfigurationUpdateResult);
     }
 
     function onSubmit(e) {
         var form = this;
-        return save(dom.parentWithClass(form, "page")), e.preventDefault(), !1
+        save(dom.parentWithClass(form, "page"));
+        e.preventDefault();
+        return false;
     }
-    return function(view, params) {
-        view.querySelector("form").addEventListener("submit", onSubmit), view.addEventListener("viewshow", loadData)
-    }
+
+    return function (view, params) {
+        view.querySelector("form").addEventListener("submit", onSubmit);
+        view.addEventListener("viewshow", loadData);
+    };
 });
