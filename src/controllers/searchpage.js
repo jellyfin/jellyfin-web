0d2ac4c6ab6bcbb56e02f4ee0263607534f16ab8
grafixeyehero <grafixeyehero>
2019-10-09 03:08:14 +0300

    searchpage

--- src/controllers/searchpage.js
+++ src/controllers/searchpage.js
@@ -1,21 +1,36 @@
-define(["focusManager", "searchFields", "searchResults", "events"], function(focusManager, SearchFields, SearchResults, events) {
+define(["focusManager", "searchFields", "searchResults", "events"], function (focusManager, SearchFields, SearchResults, events) {
     "use strict";
-    return function(view, params) {
+
+    return function (view, params) {
         function onSearch(e, value) {
-            self.searchResults.search(value)
+            self.searchResults.search(value);
         }
+
         var self = this;
-        view.addEventListener("viewshow", function() {
-            self.searchFields || (self.searchFields = new SearchFields({
-                element: view.querySelector(".searchFields")
-            }), self.searchResults = new SearchResults({
-                element: view.querySelector(".searchResults"),
-                serverId: params.serverId || ApiClient.serverId(),
-                parentId: params.parentId,
-                collectionType: params.collectionType
-            }), events.on(self.searchFields, "search", onSearch))
-        }), view.addEventListener("viewdestroy", function() {
-            self.searchFields && (self.searchFields.destroy(), self.searchFields = null), self.searchResults && (self.searchResults.destroy(), self.searchResults = null)
-        })
-    }
+        view.addEventListener("viewshow", function () {
+            if (!self.searchFields) {
+                self.searchFields = new SearchFields({
+                    element: view.querySelector(".searchFields")
+                });
+                self.searchResults = new SearchResults({
+                    element: view.querySelector(".searchResults"),
+                    serverId: params.serverId || ApiClient.serverId(),
+                    parentId: params.parentId,
+                    collectionType: params.collectionType
+                });
+                events.on(self.searchFields, "search", onSearch);
+            }
+        });
+        view.addEventListener("viewdestroy", function () {
+            if (self.searchFields) {
+                self.searchFields.destroy();
+                self.searchFields = null;
+            }
+
+            if (self.searchResults) {
+                self.searchResults.destroy();
+                self.searchResults = null;
+            }
+        });
+    };
 });
