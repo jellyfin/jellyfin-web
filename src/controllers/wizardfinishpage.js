2d24d94356d2d79fcae3404cc7408159701f51ed
grafixeyehero <grafixeyehero>
2019-10-09 03:19:42 +0300

    wizardfinishpage

--- src/controllers/wizardfinishpage.js
+++ src/controllers/wizardfinishpage.js
@@ -1,22 +1,24 @@
-define(["loading"], function(loading) {
+define(["loading"], function (loading) {
     "use strict";
 
     function onFinish() {
-        loading.show(), ApiClient.ajax({
+        loading.show();
+        ApiClient.ajax({
             url: ApiClient.getUrl("Startup/Complete"),
             type: "POST"
-        }).then(function() {
+        }).then(function () {
             Dashboard.navigate("dashboard.html");
             loading.hide();
         });
     }
-    return function(view, params) {
+
+    return function (view, params) {
         view.querySelector(".btnWizardNext").addEventListener("click", onFinish);
-        view.addEventListener("viewshow", function() {
-            document.querySelector(".skinHeader").classList.add("noHomeButtonHeader")
+        view.addEventListener("viewshow", function () {
+            document.querySelector(".skinHeader").classList.add("noHomeButtonHeader");
         });
-        view.addEventListener("viewhide", function() {
-            document.querySelector(".skinHeader").classList.remove("noHomeButtonHeader")
+        view.addEventListener("viewhide", function () {
+            document.querySelector(".skinHeader").classList.remove("noHomeButtonHeader");
         });
     };
 });
