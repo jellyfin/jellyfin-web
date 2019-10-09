05cd881f767aae732be6cd9a96bacb1013c2f4c3
grafixeyehero <grafixeyehero>
2019-10-09 03:10:16 +0300

    streamingsettings

--- src/controllers/streamingsettings.js
+++ src/controllers/streamingsettings.js
@@ -1,16 +1,19 @@
-define(["jQuery", "libraryMenu", "loading"], function($, libraryMenu, loading) {
+define(["jQuery", "libraryMenu", "loading"], function ($, libraryMenu, loading) {
     "use strict";
 
     function loadPage(page, config) {
-        $("#txtRemoteClientBitrateLimit", page).val(config.RemoteClientBitrateLimit / 1e6 || ""), loading.hide()
+        $("#txtRemoteClientBitrateLimit", page).val(config.RemoteClientBitrateLimit / 1e6 || "");
+        loading.hide();
     }
 
     function onSubmit() {
         loading.show();
         var form = this;
-        return ApiClient.getServerConfiguration().then(function(config) {
-            config.RemoteClientBitrateLimit = parseInt(1e6 * parseFloat($("#txtRemoteClientBitrateLimit", form).val() || "0")), ApiClient.updateServerConfiguration(config).then(Dashboard.processServerConfigurationUpdateResult)
-        }), !1
+        ApiClient.getServerConfiguration().then(function (config) {
+            config.RemoteClientBitrateLimit = parseInt(1e6 * parseFloat($("#txtRemoteClientBitrateLimit", form).val() || "0"));
+            ApiClient.updateServerConfiguration(config).then(Dashboard.processServerConfigurationUpdateResult);
+        });
+        return false;
     }
 
     function getTabs() {
@@ -23,30 +26,35 @@ define(["jQuery", "libraryMenu", "loading"], function($, libraryMenu, loading) {
         }, {
             href: "streamingsettings.html",
             name: Globalize.translate("TabStreaming")
-        }]
+        }];
     }
 
-    $(document).on("pageinit", "#streamingSettingsPage", function() {
+    $(document).on("pageinit", "#streamingSettingsPage", function () {
         var page = this;
-        $("#btnSelectTranscodingTempPath", page).on("click.selectDirectory", function() {
-            require(["directorybrowser"], function(directoryBrowser) {
-                var picker = new directoryBrowser;
+        $("#btnSelectTranscodingTempPath", page).on("click.selectDirectory", function () {
+            require(["directorybrowser"], function (directoryBrowser) {
+                var picker = new directoryBrowser();
                 picker.show({
-                    callback: function(path) {
-                        path && $("#txtTranscodingTempPath", page).val(path), picker.close()
+                    callback: function (path) {
+                        if (path) {
+                            $("#txtTranscodingTempPath", page).val(path);
+                        }
+
+                        picker.close();
                     },
-                    validateWriteable: !0,
+                    validateWriteable: true,
                     header: Globalize.translate("HeaderSelectTranscodingPath"),
                     instruction: Globalize.translate("HeaderSelectTranscodingPathHelp")
-                })
-            })
-        }), $(".streamingSettingsForm").off("submit", onSubmit).on("submit", onSubmit)
-    }).on("pageshow", "#streamingSettingsPage", function() {
+                });
+            });
+        });
+        $(".streamingSettingsForm").off("submit", onSubmit).on("submit", onSubmit);
+    }).on("pageshow", "#streamingSettingsPage", function () {
         loading.show();
         libraryMenu.setTabs("playback", 2, getTabs);
         var page = this;
-        ApiClient.getServerConfiguration().then(function(config) {
-            loadPage(page, config)
-        })
-    })
+        ApiClient.getServerConfiguration().then(function (config) {
+            loadPage(page, config);
+        });
+    });
 });
