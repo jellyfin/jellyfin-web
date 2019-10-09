6aa16bf77a7fb3e0f1ca561422447ece2f814c07
grafixeyehero <grafixeyehero>
2019-10-09 18:43:04 +0300

    tvupcoming

--- src/controllers/shows/tvupcoming.js
+++ src/controllers/shows/tvupcoming.js
@@ -18,13 +18,14 @@ define(["layoutManager", "loading", "datetime", "libraryBrowser", "cardBuilder",
     function loadUpcoming(context, params, promise) {
         promise.then(function (result) {
             var items = result.Items;
+
             if (items.length) {
                 context.querySelector(".noItemsMessage").style.display = "none";
             } else {
                 context.querySelector(".noItemsMessage").style.display = "block";
             }
-            var elem = context.querySelector("#upcomingItems");
-            renderUpcoming(elem, items);
+
+            renderUpcoming(context.querySelector("#upcomingItems"), items);
             loading.hide();
         });
     }
@@ -38,27 +39,27 @@ define(["layoutManager", "loading", "datetime", "libraryBrowser", "cardBuilder",
     }
 
     function renderUpcoming(elem, items) {
+        var i;
+        var length;
         var groups = [];
         var currentGroupName = "";
         var currentGroup = [];
-        var i, length;
+
         for (i = 0, length = items.length; i < length; i++) {
             var item = items[i];
             var dateText = "";
+
             if (item.PremiereDate) {
                 try {
                     var premiereDate = datetime.parseISO8601Date(item.PremiereDate, true);
-                    if (datetime.isRelativeDay(premiereDate, -1)) {
-                        dateText = Globalize.translate("Yesterday");
-                    } else {
-                        dateText = datetime.toLocaleDateString(premiereDate, {
-                            weekday: "long",
-                            month: "short",
-                            day: "numeric"
-                        });
-                    }
+                    dateText = datetime.isRelativeDay(premiereDate, -1) ? Globalize.translate("Yesterday") : datetime.toLocaleDateString(premiereDate, {
+                        weekday: "long",
+                        month: "short",
+                        day: "numeric"
+                    });
                 } catch (err) {}
             }
+
             if (dateText != currentGroupName) {
                 if (currentGroup.length) {
                     groups.push({
@@ -66,25 +67,35 @@ define(["layoutManager", "loading", "datetime", "libraryBrowser", "cardBuilder",
                         items: currentGroup
                     });
                 }
+
                 currentGroupName = dateText;
                 currentGroup = [item];
             } else {
                 currentGroup.push(item);
             }
         }
+
         var html = "";
+
         for (i = 0, length = groups.length; i < length; i++) {
             var group = groups[i];
             html += '<div class="verticalSection">';
             html += '<h2 class="sectionTitle sectionTitle-cards padded-left">' + group.name + "</h2>";
             var allowBottomPadding = true;
+
             if (enableScrollX()) {
                 allowBottomPadding = false;
                 var scrollXClass = "scrollX hiddenScrollX";
+
+                if (layoutManager.tv) {
+                    scrollXClass += " smoothScrollX";
+                }
+
                 html += '<div is="emby-itemscontainer" class="itemsContainer ' + scrollXClass + ' padded-left padded-right">';
             } else {
                 html += '<div is="emby-itemscontainer" class="itemsContainer vertical-wrap padded-left padded-right">';
             }
+
             var supportsImageAnalysis = appHost.supports("imageanalysis");
             supportsImageAnalysis = false;
             html += cardBuilder.getCardsHtml({
@@ -106,16 +117,19 @@ define(["layoutManager", "loading", "datetime", "libraryBrowser", "cardBuilder",
             html += "</div>";
             html += "</div>";
         }
+
         elem.innerHTML = html;
         imageLoader.lazyChildren(elem);
     }
 
     return function (view, params, tabContent) {
-        var self = this;
         var upcomingPromise;
+        var self = this;
+
         self.preRender = function () {
             upcomingPromise = getUpcomingPromise(view, params);
         };
+
         self.renderTab = function () {
             loadUpcoming(tabContent, params, upcomingPromise);
         };
