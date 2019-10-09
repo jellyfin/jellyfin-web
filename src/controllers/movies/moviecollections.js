e25b2865dea6f66ca441a4a71130d7be942f301e
grafixeyehero <grafixeyehero>
2019-10-09 03:26:56 +0300

    moviecollections

--- src/controllers/movies/moviecollections.js
+++ src/controllers/movies/moviecollections.js
@@ -1,9 +1,11 @@
-define(["loading", "events", "libraryBrowser", "imageLoader", "listView", "cardBuilder", "apphost", "emby-itemscontainer"], function(loading, events, libraryBrowser, imageLoader, listView, cardBuilder, appHost) {
+define(["loading", "events", "libraryBrowser", "imageLoader", "listView", "cardBuilder", "apphost", "emby-itemscontainer"], function (loading, events, libraryBrowser, imageLoader, listView, cardBuilder, appHost) {
     "use strict";
-    return function(view, params, tabContent) {
+
+    return function (view, params, tabContent) {
         function getPageData(context) {
-            var key = getSavedQueryKey(context),
-                pageData = data[key];
+            var key = getSavedQueryKey(context);
+            var pageData = data[key];
+
             if (!pageData) {
                 pageData = data[key] = {
                     query: {
@@ -22,71 +24,95 @@ define(["loading", "events", "libraryBrowser", "imageLoader", "listView", "cardB
                 pageData.query.ParentId = params.topParentId;
                 libraryBrowser.loadSavedQueryValues(key, pageData.query);
             }
+
             return pageData;
         }
 
         function getQuery(context) {
-            return getPageData(context).query
+            return getPageData(context).query;
         }
 
         function getSavedQueryKey(context) {
-            return context.savedQueryKey || (context.savedQueryKey = libraryBrowser.getSavedQueryKey("moviecollections")), context.savedQueryKey
+            if (!context.savedQueryKey) {
+                context.savedQueryKey = libraryBrowser.getSavedQueryKey("moviecollections");
+            }
+
+            return context.savedQueryKey;
         }
 
         function onViewStyleChange() {
-            var viewStyle = self.getCurrentViewStyle(),
-                itemsContainer = tabContent.querySelector(".itemsContainer");
-            "List" == viewStyle ? (itemsContainer.classList.add("vertical-list"), itemsContainer.classList.remove("vertical-wrap")) : (itemsContainer.classList.remove("vertical-list"), itemsContainer.classList.add("vertical-wrap")), itemsContainer.innerHTML = ""
+            var viewStyle = self.getCurrentViewStyle();
+            var itemsContainer = tabContent.querySelector(".itemsContainer");
+
+            if ("List" == viewStyle) {
+                itemsContainer.classList.add("vertical-list");
+                itemsContainer.classList.remove("vertical-wrap");
+            } else {
+                itemsContainer.classList.remove("vertical-list");
+                itemsContainer.classList.add("vertical-wrap");
+            }
+
+            itemsContainer.innerHTML = "";
         }
 
         function reloadItems(page) {
             loading.show();
             isLoading = true;
             var query = getQuery(page);
-            ApiClient.getItems(ApiClient.getCurrentUserId(), query).then(function(result) {
+            ApiClient.getItems(ApiClient.getCurrentUserId(), query).then(function (result) {
                 function onNextPageClick() {
-                    if (isLoading) return;
-                    query.StartIndex += query.Limit, reloadItems(tabContent)
+                    if (isLoading) {
+                        return;
+                    }
+
+                    query.StartIndex += query.Limit;
+                    reloadItems(tabContent);
                 }
 
                 function onPreviousPageClick() {
-                    if (isLoading) return;
-                    query.StartIndex -= query.Limit, reloadItems(tabContent)
+                    if (isLoading) {
+                        return;
+                    }
+
+                    query.StartIndex -= query.Limit;
+                    reloadItems(tabContent);
                 }
+
                 window.scrollTo(0, 0);
-                var html, pagingHtml = libraryBrowser.getQueryPagingHtml({
-                        startIndex: query.StartIndex,
-                        limit: query.Limit,
-                        totalRecordCount: result.TotalRecordCount,
-                        showLimit: !1,
-                        updatePageSizeSetting: !1,
-                        addLayoutButton: !1,
-                        sortButton: !1,
-                        filterButton: !1
-                    }),
-                    viewStyle = self.getCurrentViewStyle();
+                var html;
+                var pagingHtml = libraryBrowser.getQueryPagingHtml({
+                    startIndex: query.StartIndex,
+                    limit: query.Limit,
+                    totalRecordCount: result.TotalRecordCount,
+                    showLimit: false,
+                    updatePageSizeSetting: false,
+                    addLayoutButton: false,
+                    sortButton: false,
+                    filterButton: false
+                });
+                var viewStyle = self.getCurrentViewStyle();
                 html = "Thumb" == viewStyle ? cardBuilder.getCardsHtml({
                     items: result.Items,
                     shape: "backdrop",
-                    preferThumb: !0,
+                    preferThumb: true,
                     context: "movies",
-                    overlayPlayButton: !0,
-                    centerText: !0,
-                    showTitle: !0
+                    overlayPlayButton: true,
+                    centerText: true,
+                    showTitle: true
                 }) : "ThumbCard" == viewStyle ? cardBuilder.getCardsHtml({
                     items: result.Items,
                     shape: "backdrop",
-                    preferThumb: !0,
+                    preferThumb: true,
                     context: "movies",
-                    lazy: !0,
-                    cardLayout: !0,
-                    showTitle: !0
+                    lazy: true,
+                    cardLayout: true,
+                    showTitle: true
                 }) : "Banner" == viewStyle ? cardBuilder.getCardsHtml({
                     items: result.Items,
                     shape: "banner",
-                    preferBanner: !0,
+                    preferBanner: true,
                     context: "movies",
-                    lazy: !0
+                    lazy: true
                 }) : "List" == viewStyle ? listView.getListViewHtml({
                     items: result.Items,
                     context: "movies",
@@ -95,80 +121,111 @@ define(["loading", "events", "libraryBrowser", "imageLoader", "listView", "cardB
                     items: result.Items,
                     shape: "auto",
                     context: "movies",
-                    showTitle: !0,
-                    centerText: !1,
-                    cardLayout: !0
+                    showTitle: true,
+                    centerText: false,
+                    cardLayout: true
                 }) : cardBuilder.getCardsHtml({
                     items: result.Items,
                     shape: "auto",
                     context: "movies",
-                    centerText: !0,
-                    overlayPlayButton: !0,
-                    showTitle: !0
+                    centerText: true,
+                    overlayPlayButton: true,
+                    showTitle: true
                 });
-                var i, length, elems = tabContent.querySelectorAll(".paging");
-                for (i = 0, length = elems.length; i < length; i++) elems[i].innerHTML = pagingHtml;
-                for (elems = tabContent.querySelectorAll(".btnNextPage"), i = 0, length = elems.length; i < length; i++) elems[i].addEventListener("click", onNextPageClick);
-                for (elems = tabContent.querySelectorAll(".btnPreviousPage"), i = 0, length = elems.length; i < length; i++) elems[i].addEventListener("click", onPreviousPageClick);
-                result.Items.length || (html = '<p style="text-align:center;">' + Globalize.translate("MessageNoCollectionsAvailable") + "</p>");
+                var i;
+                var length;
+                var elems = tabContent.querySelectorAll(".paging");
+
+                for (i = 0, length = elems.length; i < length; i++) {
+                    elems[i].innerHTML = pagingHtml;
+                }
+
+                for (elems = tabContent.querySelectorAll(".btnNextPage"), i = 0, length = elems.length; i < length; i++) {
+                    elems[i].addEventListener("click", onNextPageClick);
+                }
+
+                for (elems = tabContent.querySelectorAll(".btnPreviousPage"), i = 0, length = elems.length; i < length; i++) {
+                    elems[i].addEventListener("click", onPreviousPageClick);
+                }
+
+                if (!result.Items.length) {
+                    html = '<p style="text-align:center;">' + Globalize.translate("MessageNoCollectionsAvailable") + "</p>";
+                }
+
                 var itemsContainer = tabContent.querySelector(".itemsContainer");
                 itemsContainer.innerHTML = html;
                 imageLoader.lazyChildren(itemsContainer);
                 libraryBrowser.saveQueryValues(getSavedQueryKey(page), query);
                 loading.hide();
                 isLoading = false;
-            })
+            });
         }
-        var self = this,
-            pageSize = 100,
-            data = {},
-            isLoading = false;
-        self.getCurrentViewStyle = function() {
-                return getPageData(tabContent).view
-            },
-            function(tabContent) {
-                tabContent.querySelector(".btnSort").addEventListener("click", function(e) {
-                    libraryBrowser.showSortMenu({
-                        items: [{
-                            name: Globalize.translate("OptionNameSort"),
-                            id: "SortName"
-                        }, {
-                            name: Globalize.translate("OptionImdbRating"),
-                            id: "CommunityRating,SortName"
-                        }, {
-                            name: Globalize.translate("OptionDateAdded"),
-                            id: "DateCreated,SortName"
-                        }, {
-                            name: Globalize.translate("OptionParentalRating"),
-                            id: "OfficialRating,SortName"
-                        }, {
-                            name: Globalize.translate("OptionReleaseDate"),
-                            id: "PremiereDate,SortName"
-                        }],
-                        callback: function() {
-                            getQuery(tabContent).StartIndex = 0, reloadItems(tabContent)
-                        },
-                        query: getQuery(tabContent),
-                        button: e.target
-                    })
+
+        var self = this;
+        var pageSize = 100;
+        var data = {};
+        var isLoading = false;
+
+        self.getCurrentViewStyle = function () {
+            return getPageData(tabContent).view;
+        };
+
+        (function (tabContent) {
+            tabContent.querySelector(".btnSort").addEventListener("click", function (e) {
+                libraryBrowser.showSortMenu({
+                    items: [{
+                        name: Globalize.translate("OptionNameSort"),
+                        id: "SortName"
+                    }, {
+                        name: Globalize.translate("OptionImdbRating"),
+                        id: "CommunityRating,SortName"
+                    }, {
+                        name: Globalize.translate("OptionDateAdded"),
+                        id: "DateCreated,SortName"
+                    }, {
+                        name: Globalize.translate("OptionParentalRating"),
+                        id: "OfficialRating,SortName"
+                    }, {
+                        name: Globalize.translate("OptionReleaseDate"),
+                        id: "PremiereDate,SortName"
+                    }],
+                    callback: function () {
+                        getQuery(tabContent).StartIndex = 0;
+                        reloadItems(tabContent);
+                    },
+                    query: getQuery(tabContent),
+                    button: e.target
                 });
-                var btnSelectView = tabContent.querySelector(".btnSelectView");
-                btnSelectView.addEventListener("click", function(e) {
-                    libraryBrowser.showLayoutMenu(e.target, self.getCurrentViewStyle(), "List,Poster,PosterCard,Thumb,ThumbCard".split(","))
-                }), btnSelectView.addEventListener("layoutchange", function(e) {
-                    var viewStyle = e.detail.viewStyle;
-                    getPageData(tabContent).view = viewStyle, libraryBrowser.saveViewSetting(getSavedQueryKey(tabContent), viewStyle), getQuery(tabContent).StartIndex = 0, onViewStyleChange(), reloadItems(tabContent)
-                }), tabContent.querySelector(".btnNewCollection").addEventListener("click", function() {
-                    require(["collectionEditor"], function(collectionEditor) {
-                        var serverId = ApiClient.serverInfo().Id;
-                        (new collectionEditor).show({
-                            items: [],
-                            serverId: serverId
-                        })
-                    })
-                })
-            }(tabContent), onViewStyleChange(), self.renderTab = function() {
-                reloadItems(tabContent)
-            }, self.destroy = function() {}
-    }
+            });
+            var btnSelectView = tabContent.querySelector(".btnSelectView");
+            btnSelectView.addEventListener("click", function (e) {
+                libraryBrowser.showLayoutMenu(e.target, self.getCurrentViewStyle(), "List,Poster,PosterCard,Thumb,ThumbCard".split(","));
+            });
+            btnSelectView.addEventListener("layoutchange", function (e) {
+                var viewStyle = e.detail.viewStyle;
+                getPageData(tabContent).view = viewStyle;
+                libraryBrowser.saveViewSetting(getSavedQueryKey(tabContent), viewStyle);
+                getQuery(tabContent).StartIndex = 0;
+                onViewStyleChange();
+                reloadItems(tabContent);
+            });
+            tabContent.querySelector(".btnNewCollection").addEventListener("click", function () {
+                require(["collectionEditor"], function (collectionEditor) {
+                    var serverId = ApiClient.serverInfo().Id;
+                    new collectionEditor().show({
+                        items: [],
+                        serverId: serverId
+                    });
+                });
+            });
+        })(tabContent);
+
+        onViewStyleChange();
+
+        self.renderTab = function () {
+            reloadItems(tabContent);
+        };
+
+        self.destroy = function () {};
+    };
 });
