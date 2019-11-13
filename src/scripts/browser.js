cdfa115ec0772da9af1beaf7213d9c7f3d7bbf53
Dmitry Lyzo <ashephard0@gmail.com>
2019-11-13 22:00:27 +0300

    Fix iOS platform detection #572

--- src/scripts/browser.js
+++ src/scripts/browser.js
@@ -122,7 +122,7 @@ define([], function () {
     }
 
     function iOSversion() {
-        if (/iP(hone|od|ad)/.test(navigator.platform)) {
+        if (/iP(hone|od|ad)|MacIntel/.test(navigator.platform)) {
             // supports iOS 2.0 and later: <http://bit.ly/TJjs1V>
             var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
             return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
