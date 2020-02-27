db4787a2599b0a06adc7339c77d8b783a37c248f
MrTimscampi <julien.machiels@protonmail.com>
2020-02-27 20:05:33 +0100

    Add image minifier and copy stray files (gulp)

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,21 @@
-"use strict";
+'use strict';
 
 const { src, dest, series, parallel } = require('gulp');
 const del = require('del');
-const babel = require("gulp-babel");
+const babel = require('gulp-babel');
 const terser = require('gulp-terser');
 const cssnano = require('gulp-cssnano');
 const htmlmin = require('gulp-htmlmin');
+const imagemin = require('gulp-imagemin');
 const sourcemaps = require('gulp-sourcemaps');
 
 // Clean assets
 function clean() {
-    return del(["dist/"]);
+    return del(['dist/']);
 }
 
 function javascript() {
-    return src("src/**/*.js", {base: './src/'})
+    return src('src/**/*.js', {base: './src/'})
     .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(babel({
         presets: ['@babel/preset-env']
@@ -24,21 +25,32 @@ function javascript() {
         mangle: false
     }))
     .pipe(sourcemaps.write('.'))
-    .pipe(dest("dist/"));
+    .pipe(dest('dist/'));
 }
 
 function css() {
-    return src("src/**/*.css", {base: './src/'})
+    return src('src/**/*.css', {base: './src/'})
     .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(cssnano())
     .pipe(sourcemaps.write('.'))
-    .pipe(dest("dist/"));
+    .pipe(dest('dist/'));
 }
 
 function html() {
-    return src("src/**/*.html", {base: './src/'})
+    return src('src/**/*.html', {base: './src/'})
     .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(dest("dist/"));
+    .pipe(dest('dist/'));
 }
 
-exports.default = series(clean, parallel(javascript, css, html))
+function images() {
+    return src(['src/**/*.png', 'src/**/*.jpg', 'src/**/*.gif', 'src/**/*.svg'], {base: './src/'})
+    .pipe(imagemin())
+    .pipe(dest('dist/'))
+}
+
+function copy() {
+    return src(['src/**/*.json', 'src/**/*.ico'], {base: './src/'})
+    .pipe(dest('dist/'))
+}
+
+exports.default = series(clean, parallel(javascript, css, html, images, copy))
