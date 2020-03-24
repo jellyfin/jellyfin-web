c882c264a30bc83a04bdf0761db09831d55bf2b8
Dmitry Lyzo <ashephard0@gmail.com>
2020-03-24 10:03:51 +0300

    Fix view restoring

--- src/components/appRouter.js
+++ src/components/appRouter.js
@@ -511,9 +511,16 @@ define(['loading', 'globalize', 'events', 'viewManager', 'layoutManager', 'skinM
         return baseRoute;
     }
 
+    var popstateOccurred = false;
+    window.addEventListener('popstate', function () {
+        popstateOccurred = true;
+    });
+
     function getHandler(route) {
         return function (ctx, next) {
+            ctx.isBack = popstateOccurred;
             handleRoute(ctx, next, route);
+            popstateOccurred = false;
         };
     }
 
