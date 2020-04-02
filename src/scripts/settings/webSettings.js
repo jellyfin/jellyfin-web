0a6fefd417700f82b84c5696f32acf737579cdbb
dkanada <dkanada@users.noreply.github.com>
2020-04-02 19:50:09 +0900

    assign data before returning value

--- src/scripts/settings/webSettings.js
+++ src/scripts/settings/webSettings.js
@@ -3,7 +3,8 @@ let data;
 function getConfig() {
     if (data) return Promise.resolve(data);
     return fetch("/config.json?nocache=" + new Date().getUTCMilliseconds()).then(function (response) {
-        return response.json();
+        data = response.json();
+        return data;
     });
 }
 
