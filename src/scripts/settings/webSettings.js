31048d0ce508ff08a51b1045a8329822f452c320
dkanada <dkanada@users.noreply.github.com>
2020-04-02 19:44:21 +0900

    only load the config once per session

--- src/scripts/settings/webSettings.js
+++ src/scripts/settings/webSettings.js
@@ -1,4 +1,7 @@
+let data;
+
 function getConfig() {
+    if (data) return Promise.resolve(data);
     return fetch("/config.json?nocache=" + new Date().getUTCMilliseconds()).then(function (response) {
         return response.json();
     });
