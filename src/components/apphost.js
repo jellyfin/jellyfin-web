51722fd2257eeb493b30b16d36556e2157e4211e
Joshua M. Boniface <joshua@boniface.me>
2020-04-12 19:31:34 -0400

    Bump version to 10.5.4

--- src/components/apphost.js
+++ src/components/apphost.js
@@ -348,7 +348,7 @@ define(["appSettings", "browser", "events", "htmlMediaHelper"], function (appSet
     var deviceId;
     var deviceName;
     var appName = "Jellyfin Web";
-    var appVersion = "10.5.3";
+    var appVersion = "10.5.4";
     var visibilityChange;
     var visibilityState;
 
