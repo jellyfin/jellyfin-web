c5669eca4ddef4342eacfc955764f5a60183fcb5
redSpoutnik <15638041+redSpoutnik@users.noreply.github.com>
2020-04-12 15:22:17 +0200

    subtitle-sync: use currentSubtitlesOctopus instead of currentAssRenderer

--- src/components/htmlvideoplayer/plugin.js
+++ src/components/htmlvideoplayer/plugin.js
@@ -600,8 +600,9 @@ define(['browser', 'require', 'events', 'apphost', 'loading', 'dom', 'playbackMa
             var offsetValue = parseFloat(offset);
 
             // if .ass currently rendering
-            if (currentAssRenderer) {
+            if (currentSubtitlesOctopus) {
                 updateCurrentTrackOffset(offsetValue);
+                currentSubtitlesOctopus.timeOffset = offsetValue;
             } else {
                 var trackElement = getTextTrack();
                 // if .vtt currently rendering
@@ -1219,11 +1220,6 @@ define(['browser', 'require', 'events', 'apphost', 'loading', 'dom', 'playbackMa
         }
 
         function updateSubtitleText(timeMs) {
-
-            // handle offset for ass tracks
-            if (currentTrackOffset) {
-                timeMs += (currentTrackOffset * 1000);
-            }
 
             var clock = currentClock;
             if (clock) {
