4c9119d4b467c473bce5c61fbde513d7dd57a77a
Brissot <sidsu7@gmail.com>
2020-05-01 16:56:01 -0400

    Made an webm AV1 profile to fix issue where AV1 would transcode instead of direct play even if the browser supports decoding AV1.

--- src/scripts/browserdeviceprofile.js
+++ src/scripts/browserdeviceprofile.js
@@ -334,6 +334,7 @@ define(['browser'], function (browser) {
 
         var canPlayVp8 = videoTestElement.canPlayType('video/webm; codecs="vp8"').replace(/no/, '');
         var canPlayVp9 = videoTestElement.canPlayType('video/webm; codecs="vp9"').replace(/no/, '');
+        var canPlayAv1 = videoTestElement.canPlayType('video/webm; codecs="av1"').replace(/no/, '');
         var webmAudioCodecs = ['vorbis'];
 
         var canPlayMkv = testCanPlayMkv(videoTestElement);
@@ -589,6 +590,15 @@ define(['browser'], function (browser) {
                 Type: 'Video',
                 AudioCodec: webmAudioCodecs.join(','),
                 VideoCodec: 'VP9'
+            });
+        }
+
+        if (canPlayAv1) {
+            profile.DirectPlayProfiles.push({
+                Container: 'webm',
+                Type: 'Video',
+                AudioCodec: webmAudioCodecs.join(','),
+                VideoCodec: 'AV1'
             });
         }
 
