63816ef69bcf8e89d89972c2c9aadc0d95143f44
Sarab Singh <sarab.singh97@gmail.com>
2020-05-03 20:39:02 +0530

    package

--- src/components/playMenu.js
+++ src/components/playMenu.js
@@ -1,75 +1,77 @@
-define(['actionsheet', 'datetime', 'playbackManager', 'globalize', 'appSettings'], function (actionsheet, datetime, playbackManager, globalize, appSettings) {
-    'use strict';
+import actionsheet from 'actionsheet';
+import datetime from 'datetime';
+import playbackManager from 'playbackManager';
+import globalize from 'globalize';
+// import appSettings from 'appSettings';
 
-    function show(options) {
+export function show(options) {
 
-        var item = options.item;
+    var item = options.item;
 
-        var itemType = item.Type;
-        var isFolder = item.IsFolder;
-        var itemId = item.Id;
-        var channelId = item.ChannelId;
-        var serverId = item.ServerId;
-        var resumePositionTicks = item.UserData ? item.UserData.PlaybackPositionTicks : null;
+    var itemType = item.Type;
+    var isFolder = item.IsFolder;
+    var itemId = item.Id;
+    var channelId = item.ChannelId;
+    var serverId = item.ServerId;
+    var resumePositionTicks = item.UserData ? item.UserData.PlaybackPositionTicks : null;
 
-        var playableItemId = itemType === 'Program' ? channelId : itemId;
+    var playableItemId = itemType === 'Program' ? channelId : itemId;
 
-        if (!resumePositionTicks || isFolder) {
+    if (!resumePositionTicks || isFolder) {
+        playbackManager.play({
+            ids: [playableItemId],
+            serverId: serverId
+        });
+        return;
+    }
+
+    var menuItems = [];
+
+    menuItems.push({
+        name: globalize.translate('ResumeAt', datetime.getDisplayRunningTime(resumePositionTicks)),
+        id: 'resume'
+    });
+
+    menuItems.push({
+        name: globalize.translate('PlayFromBeginning'),
+        id: 'play'
+    });
+
+    actionsheet.show({
+
+        items: menuItems,
+        positionTo: options.positionTo
+
+    }).then(function (id) {
+        switch (id) {
+
+            case 'play':
             playbackManager.play({
                 ids: [playableItemId],
                 serverId: serverId
             });
-            return;
+            break;
+            case 'resume':
+            playbackManager.play({
+                ids: [playableItemId],
+                startPositionTicks: resumePositionTicks,
+                serverId: serverId
+            });
+            break;
+            case 'queue':
+            playbackManager.queue({
+                items: [item]
+            });
+            break;
+            case 'shuffle':
+            playbackManager.shuffle(item);
+            break;
+            default:
+            break;
         }
+    });
+}
 
-        var menuItems = [];
-
-        menuItems.push({
-            name: globalize.translate('ResumeAt', datetime.getDisplayRunningTime(resumePositionTicks)),
-            id: 'resume'
-        });
-
-        menuItems.push({
-            name: globalize.translate('PlayFromBeginning'),
-            id: 'play'
-        });
-
-        actionsheet.show({
-
-            items: menuItems,
-            positionTo: options.positionTo
-
-        }).then(function (id) {
-            switch (id) {
-
-                case 'play':
-                    playbackManager.play({
-                        ids: [playableItemId],
-                        serverId: serverId
-                    });
-                    break;
-                case 'resume':
-                    playbackManager.play({
-                        ids: [playableItemId],
-                        startPositionTicks: resumePositionTicks,
-                        serverId: serverId
-                    });
-                    break;
-                case 'queue':
-                    playbackManager.queue({
-                        items: [item]
-                    });
-                    break;
-                case 'shuffle':
-                    playbackManager.shuffle(item);
-                    break;
-                default:
-                    break;
-            }
-        });
-    }
-
-    return {
-        show: show
-    };
-});
+export default {
+    show: show
+};
