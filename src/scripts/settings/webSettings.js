ee82cbba871b098e34dc1cb0f1ba1664150d2474
dkanada <dkanada@users.noreply.github.com>
2020-05-13 10:50:04 +0900

    move console warning to catch block

--- src/scripts/settings/webSettings.js
+++ src/scripts/settings/webSettings.js
@@ -6,12 +6,12 @@ function getConfig() {
         data = response.json();
         return data;
     }).catch(error => {
+        console.warn('web config file is missing so the template will be used');
         return getDefaultConfig();
     });
 }
 
 function getDefaultConfig() {
-    console.warn('web config file is missing so the template will be used');
     return fetch('/config.template.json').then(function (response) {
         data = response.json();
         return data;
