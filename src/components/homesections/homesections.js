efabfc6f69c5bf5f217136bba8e3a489833c8e1c
MrTimscampi <julien.machiels@protonmail.com>
2020-06-20 15:29:47 +0200

    Use parent poster for items with children

--- src/components/homesections/homesections.js
+++ src/components/homesections/homesections.js
@@ -254,7 +254,7 @@ define(['connectionManager', 'cardBuilder', 'appSettings', 'dom', 'apphost', 'la
             return cardBuilder.getCardsHtml({
                 items: items,
                 shape: shape,
-                preferThumb: viewType !== 'movies' && itemType !== 'Channel' && viewType !== 'music' ? 'auto' : null,
+                preferThumb: viewType !== 'movies' && viewType !== 'tvshows' && itemType !== 'Channel' && viewType !== 'music' ? 'auto' : null,
                 showUnplayedIndicator: false,
                 showChildCountIndicator: true,
                 context: 'home',
