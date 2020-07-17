93fcca6965d99c017cd8389c68cce105970663d6
Cameron <influence365@gmail.com>
2020-07-17 17:02:49 +0100

    update paper button and button

--- src/elements/emby-button/emby-button.js
+++ src/elements/emby-button/emby-button.js
@@ -7,73 +7,69 @@ import appHost from 'apphost';
 import 'css!./emby-button';
 import 'registerElement';
 
-/* eslint-disable indent */
+const EmbyButtonPrototype = Object.create(HTMLButtonElement.prototype);
+const EmbyLinkButtonPrototype = Object.create(HTMLAnchorElement.prototype);
 
-    const EmbyButtonPrototype = Object.create(HTMLButtonElement.prototype);
-    const EmbyLinkButtonPrototype = Object.create(HTMLAnchorElement.prototype);
-
-    function onAnchorClick(e) {
-        const href = this.getAttribute('href') || '';
-        if (href !== '#') {
-            if (this.getAttribute('target')) {
-                if (!appHost.supports('targetblank')) {
-                    e.preventDefault();
-                    shell.openUrl(href);
-                }
-            } else {
-                appRouter.handleAnchorClick(e);
+function onAnchorClick(e) {
+    const href = this.getAttribute('href') || '';
+    if (href !== '#') {
+        if (this.getAttribute('target')) {
+            if (!appHost.supports('targetblank')) {
+                e.preventDefault();
+                shell.openUrl(href);
             }
         } else {
-            e.preventDefault();
+            appRouter.handleAnchorClick(e);
         }
+    } else {
+        e.preventDefault();
+    }
+}
+
+EmbyButtonPrototype.createdCallback = function () {
+    if (this.classList.contains('emby-button')) {
+        return;
     }
 
-    EmbyButtonPrototype.createdCallback = function () {
-        if (this.classList.contains('emby-button')) {
-            return;
-        }
+    this.classList.add('emby-button');
+    // TODO replace all instances of element-showfocus with this method
+    if (layoutManager.tv) {
+        // handles all special css for tv layout
+        // this method utilizes class chaining
+        this.classList.add('show-focus');
+    }
+};
 
-        this.classList.add('emby-button');
-        // TODO replace all instances of element-showfocus with this method
-        if (layoutManager.tv) {
-            // handles all special css for tv layout
-            // this method utilizes class chaining
-            this.classList.add('show-focus');
-        }
-    };
+EmbyButtonPrototype.attachedCallback = function () {
+    if (this.tagName === 'A') {
+        dom.removeEventListener(this, 'click', onAnchorClick, {});
+        dom.addEventListener(this, 'click', onAnchorClick, {});
 
-    EmbyButtonPrototype.attachedCallback = function () {
-        if (this.tagName === 'A') {
-            dom.removeEventListener(this, 'click', onAnchorClick, {});
-            dom.addEventListener(this, 'click', onAnchorClick, {});
-
-            if (this.getAttribute('data-autohide') === 'true') {
-                if (appHost.supports('externallinks')) {
-                    this.classList.remove('hide');
-                } else {
-                    this.classList.add('hide');
-                }
+        if (this.getAttribute('data-autohide') === 'true') {
+            if (appHost.supports('externallinks')) {
+                this.classList.remove('hide');
+            } else {
+                this.classList.add('hide');
             }
         }
-    };
+    }
+};
 
-    EmbyButtonPrototype.detachedCallback = function () {
-        dom.removeEventListener(this, 'click', onAnchorClick, {});
-    };
+EmbyButtonPrototype.detachedCallback = function () {
+    dom.removeEventListener(this, 'click', onAnchorClick, {});
+};
 
-    EmbyLinkButtonPrototype.createdCallback = EmbyButtonPrototype.createdCallback;
-    EmbyLinkButtonPrototype.attachedCallback = EmbyButtonPrototype.attachedCallback;
+EmbyLinkButtonPrototype.createdCallback = EmbyButtonPrototype.createdCallback;
+EmbyLinkButtonPrototype.attachedCallback = EmbyButtonPrototype.attachedCallback;
 
-    document.registerElement('emby-button', {
-        prototype: EmbyButtonPrototype,
-        extends: 'button'
-    });
+document.registerElement('emby-button', {
+    prototype: EmbyButtonPrototype,
+    extends: 'button'
+});
 
-    document.registerElement('emby-linkbutton', {
-        prototype: EmbyLinkButtonPrototype,
-        extends: 'a'
-    });
+document.registerElement('emby-linkbutton', {
+    prototype: EmbyLinkButtonPrototype,
+    extends: 'a'
+});
 
-    export default EmbyButtonPrototype;
-
-/* eslint-enable indent */
+export default EmbyButtonPrototype;
--- src/elements/emby-button/paper-icon-button-light.js
+++ src/elements/emby-button/paper-icon-button-light.js
@@ -2,21 +2,17 @@ import layoutManager from 'layoutManager';
 import 'css!./emby-button';
 import 'registerElement';
 
-/* eslint-disable indent */
+const EmbyButtonPrototype = Object.create(HTMLButtonElement.prototype);
 
-    const EmbyButtonPrototype = Object.create(HTMLButtonElement.prototype);
+EmbyButtonPrototype.createdCallback = function () {
+    this.classList.add('paper-icon-button-light');
 
-    EmbyButtonPrototype.createdCallback = function () {
-        this.classList.add('paper-icon-button-light');
+    if (layoutManager.tv) {
+        this.classList.add('show-focus');
+    }
+};
 
-        if (layoutManager.tv) {
-            this.classList.add('show-focus');
-        }
-    };
-
-    document.registerElement('paper-icon-button-light', {
-        prototype: EmbyButtonPrototype,
-        extends: 'button'
-    });
-
-/* eslint-enable indent */
+document.registerElement('paper-icon-button-light', {
+    prototype: EmbyButtonPrototype,
+    extends: 'button'
+});
