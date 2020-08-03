1d747c9252dc5927aa3a21e8b9d85270eaae13b6
matjaz321 <matjaz.zavski15@gmail.com>
2020-08-03 21:21:39 +0200

    Fixed linting errors

--- src/controllers/livetv/livetvschedule.js
+++ src/controllers/livetv/livetvschedule.js
@@ -121,4 +121,4 @@ export default function (view, params, tabContent) {
         renderActiveRecordings(tabContent, activeRecordingsPromise);
         renderUpcomingRecordings(tabContent, upcomingRecordingsPromise);
     };
-};
+}
