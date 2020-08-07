406e07154c048c1541bf97521b17445bf5ae7c27
Bill Thornton <billt2006@gmail.com>
2020-08-07 13:18:21 -0400

    Hide download button for books when not supported

--- src/controllers/itemDetails/index.js
+++ src/controllers/itemDetails/index.js
@@ -1,3 +1,4 @@
+import appHost from 'apphost';
 import loading from 'loading';
 import appRouter from 'appRouter';
 import layoutManager from 'layoutManager';
@@ -657,7 +658,7 @@ import 'emby-select';
         setPeopleHeader(page, item);
         loading.hide();
 
-        if (item.Type === 'Book') {
+        if (item.Type === 'Book' && item.CanDownload && appHost.supports('filedownload')) {
             hideAll(page, 'btnDownload', true);
         }
 
