e6159a22d81ff9a4359c613b1fbb32192091a047
MrTimscampi <julien.machiels@protonmail.com>
2020-08-09 13:24:16 +0200

    Fix themes not loading unless signed in

--- src/scripts/settings/webSettings.js
+++ src/scripts/settings/webSettings.js
@@ -1,21 +1,41 @@
+
 let data;
 
-function getConfig() {
+async function getConfig() {
     if (data) return Promise.resolve(data);
-    return fetch('config.json?nocache=' + new Date().getUTCMilliseconds()).then(response => {
-        data = response.json();
+    try {
+        const response = await fetch('config.json', {
+            cache: 'no-cache'
+        });
+
+        if (!response.ok) {
+            throw new Error('network response was not ok');
+        }
+
+        data = await response.json();
+
         return data;
-    }).catch(error => {
-        console.warn('web config file is missing so the template will be used');
+    } catch (error) {
+        console.warn('failed to fetch the web config file:', error);
         return getDefaultConfig();
-    });
+    }
 }
 
-function getDefaultConfig() {
-    return fetch('config.template.json').then(function (response) {
+async function getDefaultConfig() {
+    try {
+        const response = await fetch('config.template.json', {
+            cache: 'no-cache'
+        });
+
+        if (!response.ok) {
+            throw new Error('network response was not ok');
+        }
+
         data = response.json();
         return data;
-    });
+    } catch (error) {
+        console.error('failed to fetch the default web config file:', error);
+    }
 }
 
 export function getMultiServer() {
@@ -29,6 +49,7 @@ export function getMultiServer() {
 
 export function getThemes() {
     return getConfig().then(config => {
+        console.warn(config.themes);
         return config.themes;
     }).catch(error => {
         console.log('cannot get web config:', error);
