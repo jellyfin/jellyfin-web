96963cb8b67850466727159b6035589b08758a28
MrTimscampi <julien.machiels@protonmail.com>
2020-08-11 19:53:11 +0200

    Wrap appLoader in anonymous function

--- src/scripts/apploader.js
+++ src/scripts/apploader.js
@@ -1,48 +1,50 @@
-function injectScriptElement(src, onload) {
-    if (!src) {
-        return;
-    }
-
-    const script = document.createElement('script');
-    if (self.dashboardVersion) {
-        src += `?v=${self.dashboardVersion}`;
-    }
-    script.src = src;
-    script.setAttribute('async', '');
-
-    if (onload) {
-        script.onload = onload;
-    }
-
-    document.head.appendChild(script);
-}
-
-function loadSite() {
-    injectScriptElement(
-        './libraries/alameda.js',
-        function() {
-            // onload of require library
-            injectScriptElement('./scripts/site.js');
+(function() {
+    function injectScriptElement(src, onload) {
+        if (!src) {
+            return;
         }
-    );
-}
 
-try {
-    Promise.resolve();
-} catch (ex) {
-    // this checks for several cases actually, typical is
-    // Promise() being missing on some legacy browser, and a funky one
-    // is Promise() present but buggy on WebOS 2
-    window.Promise = undefined;
-    self.Promise = undefined;
-}
+        const script = document.createElement('script');
+        if (self.dashboardVersion) {
+            src += `?v=${self.dashboardVersion}`;
+        }
+        script.src = src;
+        script.setAttribute('async', '');
 
-if (!self.Promise) {
-    // Load Promise polyfill if they are not natively supported
-    injectScriptElement(
-        './libraries/npo.js',
-        loadSite
-    );
-} else {
-    loadSite();
-}
+        if (onload) {
+            script.onload = onload;
+        }
+
+        document.head.appendChild(script);
+    }
+
+    function loadSite() {
+        injectScriptElement(
+            './libraries/alameda.js',
+            function() {
+                // onload of require library
+                injectScriptElement('./scripts/site.js');
+            }
+        );
+    }
+
+    try {
+        Promise.resolve();
+    } catch (ex) {
+        // this checks for several cases actually, typical is
+        // Promise() being missing on some legacy browser, and a funky one
+        // is Promise() present but buggy on WebOS 2
+        window.Promise = undefined;
+        self.Promise = undefined;
+    }
+
+    if (!self.Promise) {
+        // Load Promise polyfill if they are not natively supported
+        injectScriptElement(
+            './libraries/npo.js',
+            loadSite
+        );
+    } else {
+        loadSite();
+    }
+})();
