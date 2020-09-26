6f7819366042c210c00aa46a867677a0d8519d55
Dmitry Lyzo <ashephard0@gmail.com>
2020-09-26 12:27:14 +0300

    Fix 'file:' fetching (bundled apps)

--- src/scripts/settings/webSettings.js
+++ src/scripts/settings/webSettings.js
@@ -1,9 +1,40 @@
 let data;
 
+// `fetch` with `file:` support
+// Recent browsers seem to support `file` protocol under some conditions.
+// Based on https://github.com/github/fetch/pull/92#issuecomment-174730593
+async function fetchLocal(url, options) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest;
+
+        xhr.onload = () => {
+            // `file` protocol has invalid OK status
+            let status = xhr.status;
+            if (xhr.responseURL.startsWith('file:') && status === 0) {
+                status = 200;
+            }
+
+            resolve(new Response(xhr.responseText, {status: status}));
+        }
+
+        xhr.onerror = () => {
+            reject(new TypeError('Local request failed'));
+        }
+
+        xhr.open('GET', url);
+
+        if (options && options.cache) {
+            xhr.setRequestHeader('Cache-Control', options.cache);
+        }
+
+        xhr.send(null);
+    });
+}
+
 async function getConfig() {
     if (data) return Promise.resolve(data);
     try {
-        const response = await fetch('config.json', {
+        const response = await fetchLocal('config.json', {
             cache: 'no-cache'
         });
 
@@ -22,7 +53,7 @@ async function getConfig() {
 
 async function getDefaultConfig() {
     try {
-        const response = await fetch('config.template.json', {
+        const response = await fetchLocal('config.template.json', {
             cache: 'no-cache'
         });
 
