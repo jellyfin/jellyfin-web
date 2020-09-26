78a3c971ff0e508b1fc367d25bcf3915c0bf319d
Dmitry Lyzo <ashephard0@gmail.com>
2020-09-26 21:48:22 +0300

    Fix unstyled scrollbar

--- src/scripts/autoThemes.js
+++ src/scripts/autoThemes.js
@@ -3,7 +3,11 @@ import skinManager from 'skinManager';
 import events from 'events';
 
 // Set the default theme when loading
-skinManager.setTheme(userSettings.theme());
+skinManager.setTheme(userSettings.theme())
+    /* This keeps the scrollbar always present in all pages, so we avoid clipping while switching between pages
+       that need the scrollbar and pages that don't.
+     */
+    .then(() => document.body.classList.add('force-scroll'));
 
 // Set the user's prefered theme when signing in
 events.on(window.connectionManager, 'localusersignedin', function (e, user) {
