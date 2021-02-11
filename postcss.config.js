6a0c8107d3b815825cac9b6d6f445f87bc9e6763
MrTimscampi <julien.machiels@protonmail.com>
2021-02-11 23:53:40 +0100

    Add TailwindCSS support

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,18 @@
 const packageConfig = require('./package.json');
 const postcssPresetEnv = require('postcss-preset-env');
 const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
+// Disable CSSNano temporarily since it doesn't support PostCSS 8 yet
+// const cssnano = require('cssnano');
+const tailwindcss = require('tailwindcss');
 
 const config = () => ({
     plugins: [
+        tailwindcss(),
         // Explicitly specify browserslist to override ones from node_modules
         // For example, Swiper has it in its package.json
         postcssPresetEnv({browsers: packageConfig.browserslist}),
-        autoprefixer({overrideBrowserslist: packageConfig.browserslist}),
-        cssnano()
+        autoprefixer({overrideBrowserslist: packageConfig.browserslist})
+        //cssnano()
     ]
 });
 
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    purge: [],
+    darkMode: false,
+    theme: {
+        extend: {}
+    },
+    variants: {
+        extend: {}
+    },
+    plugins: []
+};
