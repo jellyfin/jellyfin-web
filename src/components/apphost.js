65f4e29d3668cb7c00bb24bab2bbf70a5a60388e
Joshua M. Boniface <joshua@boniface.me>
2021-05-04 21:16:31 -0400

    Bump version to 10.7.4

--- src/components/apphost.js
+++ src/components/apphost.js
@@ -8,7 +8,7 @@ import globalize from '../scripts/globalize';
 import profileBuilder from '../scripts/browserDeviceProfile';
 
 const appName = 'Jellyfin Web';
-const appVersion = '10.7.3';
+const appVersion = '10.7.4';
 
 function getBaseProfileOptions(item) {
     const disableHlsVideoAudioCodecs = [];
