74940b601b919943967f4856654ada4a81fb0024
Bill Thornton <billt2006@gmail.com>
2021-06-03 10:14:28 -0400

    Move serviceworker precaching to production config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,15 @@
+const path = require('path');
 const common = require('./webpack.common');
 const merge = require('webpack-merge');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
-    entry: './scripts/site.js'
+    entry: './scripts/site.js',
+    plugins: [
+        new WorkboxPlugin.InjectManifest({
+            swSrc: path.resolve(__dirname, 'src/serviceworker.js'),
+            swDest: 'serviceworker.js'
+        })
+    ]
 });
