e64f4e6997d772a5082d2b9d5fc8d03d455776d9
MrTimscampi <julien.machiels@protonmail.com>
2021-06-18 17:46:45 +0200

    Update Tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,7 @@
 const packageConfig = require('./package.json');
 const postcssPresetEnv = require('postcss-preset-env');
 const autoprefixer = require('autoprefixer');
-// Disable CSSNano temporarily since it doesn't support PostCSS 8 yet
-// const cssnano = require('cssnano');
+const cssnano = require('cssnano');
 const tailwindcss = require('tailwindcss');
 
 const config = () => ({
@@ -11,8 +10,8 @@ const config = () => ({
         // Explicitly specify browserslist to override ones from node_modules
         // For example, Swiper has it in its package.json
         postcssPresetEnv({browsers: packageConfig.browserslist}),
-        autoprefixer({overrideBrowserslist: packageConfig.browserslist})
-        //cssnano()
+        autoprefixer({overrideBrowserslist: packageConfig.browserslist}),
+        cssnano()
     ]
 });
 
