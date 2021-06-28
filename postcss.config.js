099271a4cad88634af94cfa413e14ed5255c9d64
MrTimscampi <julien.machiels@protonmail.com>
2021-06-28 18:57:28 +0200

    Fix build

--- postcss.config.js
+++ postcss.config.js
@@ -5,11 +5,19 @@ const cssnano = require('cssnano');
 const tailwindcss = require('tailwindcss');
 
 const config = () => ({
+    syntax: 'postcss-scss',
+    parser: 'postcss-scss',
     plugins: [
         tailwindcss(),
         // Explicitly specify browserslist to override ones from node_modules
         // For example, Swiper has it in its package.json
-        postcssPresetEnv({browsers: packageConfig.browserslist}),
+        postcssPresetEnv({
+            browsers: packageConfig.browserslist,
+            features: {
+                // This causes conflicts with TailwindCSS due to it already handling this.
+                'focus-within-pseudo-class': false
+            }
+        }),
         autoprefixer({overrideBrowserslist: packageConfig.browserslist}),
         cssnano()
     ]
