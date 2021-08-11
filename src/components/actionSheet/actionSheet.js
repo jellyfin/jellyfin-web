7e68191c037553f00d15456ebcef41aec9063d50
Tyler Szabo <tyler@szabomail.ca>
2021-08-10 19:53:54 -0700

    Compute menu positions using nominal size #825
    Animation of the menu that occurred during position calculation for initial placement caused size of the menu and thus the position to be incorrectly calculated on Firefox.

--- src/components/actionSheet/actionSheet.js
+++ src/components/actionSheet/actionSheet.js
@@ -42,8 +42,8 @@ function getPosition(options, dlg) {
 
     pos.left += (pos.width || 0) / 2;
 
-    const height = dlg.offsetHeight || 300;
-    const width = dlg.offsetWidth || 160;
+    const height = dlg.nominalOffsetHeight || dlg.offsetHeight || 300;
+    const width = dlg.nominalOffsetWidth || dlg.offsetWidth || 160;
 
     // Account for popup size
     pos.top -= height / 2;
