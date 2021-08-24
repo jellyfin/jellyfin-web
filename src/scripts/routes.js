626993a740d794ce0a0d590db18c2198e595e843
Dmitry Lyzo <ashephard0@gmail.com>
2021-08-25 00:11:12 +0300

    Fix dialog history

--- src/scripts/routes.js
+++ src/scripts/routes.js
@@ -561,6 +561,11 @@ import { appRouter } from '../components/appRouter';
     });
 
     defineRoute({
+        path: '/dialog',
+        dummyRoute: true
+    });
+
+    defineRoute({
         path: '',
         isDefaultRoute: true,
         autoFocus: false
