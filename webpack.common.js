77ccce49a1aa82f5fe08057c9e2bc285b29e66b3
Bill Thornton <billt2006@gmail.com>
2021-10-23 01:24:14 -0400

    Replace file-loader with asset type

--- webpack.common.js
+++ webpack.common.js
@@ -153,17 +153,15 @@ module.exports = {
             },
             {
                 test: /\.(png|jpg|gif|svg)$/i,
-                use: ['file-loader']
+                type: 'asset/resource'
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader'
-                ]
+                type: 'asset/resource'
             },
             {
                 test: /\.(mp3)$/i,
-                use: ['file-loader']
+                type: 'asset/resource'
             },
             {
                 test: require.resolve('jquery'),
