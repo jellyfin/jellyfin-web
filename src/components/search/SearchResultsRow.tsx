8338bb550a2439ed57b635371b7f64f708b43cd8
Dmitry Lyzo <ashephard0@gmail.com>
2022-02-19 00:22:13 +0300

    Use type from API

--- src/components/search/SearchResultsRow.tsx
+++ src/components/search/SearchResultsRow.tsx
@@ -1,3 +1,4 @@
+import { BaseItemDto } from '@thornbill/jellyfin-sdk/dist/generated-client';
 import React, { FunctionComponent, useEffect, useRef } from 'react';
 
 import cardBuilder from '../cardbuilder/cardBuilder';
@@ -17,7 +18,7 @@ const createScroller = ({ title = '' }) => ({
 
 type SearchResultsRowProps = {
     title?: string;
-    items?: Array<any>; // TODO: Should be Array<BaseItemDto> once we have a typed API client
+    items?: BaseItemDto[];
     cardOptions?: Record<string, any>;
 }
 
