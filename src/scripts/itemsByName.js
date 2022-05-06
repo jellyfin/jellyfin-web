ee495cd41280e318eb011bd8c07a73369badf3d6
grafixeyehero <grafixeyehero@yahoo.com>
2022-05-06 23:33:06 +0300

    Remove unused parameter "page"

--- src/scripts/itemsByName.js
+++ src/scripts/itemsByName.js
@@ -89,11 +89,11 @@ function renderItems(page, item) {
     const sectionElems = elem.querySelectorAll('.verticalSection');
 
     for (let i = 0, length = sectionElems.length; i < length; i++) {
-        renderSection(page, item, sectionElems[i], sectionElems[i].getAttribute('data-type'));
+        renderSection(item, sectionElems[i], sectionElems[i].getAttribute('data-type'));
     }
 }
 
-function renderSection(page, item, element, type) {
+function renderSection(item, element, type) {
     switch (type) {
         case 'Program':
             loadItems(element, item, type, {
