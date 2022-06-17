afc29be768f4f8b56bb4de623544d9acec47068f
April <april.l.petersen@protonmail.com>
2022-06-17 10:22:47 -0500

    Added browser module

--- src/scripts/settings/userSettings.js
+++ src/scripts/settings/userSettings.js
@@ -1,6 +1,7 @@
 import appSettings from './appSettings';
 import { Events } from 'jellyfin-apiclient';
 import { toBoolean } from '../../utils/string.ts';
+import browser from '../browser';
 
 function onSaveTimeout() {
     const self = this;
