bac7d6467eef9a9031ec23eddb86b4a31151c68e
Hadi Charara <hadi.charara@protonmail.com>
2022-07-02 17:49:00 -0400

    Fixed Cards for RTL

--- src/components/indicators/indicators.js
+++ src/components/indicators/indicators.js
@@ -84,7 +84,7 @@ export function getPlayedIndicatorHtml(item) {
     if (enablePlayedIndicator(item)) {
         const userData = item.UserData || {};
         if (userData.UnplayedItemCount) {
-            return '<div class="countIndicator indicator">' + userData.UnplayedItemCount + '</div>';
+            return '<div class="countIndicator indicator">' + userData.UnplayedItemCount.toLocaleString() + '</div>';
         }
 
         if (userData.PlayedPercentage && userData.PlayedPercentage >= 100 || (userData.Played)) {
@@ -99,7 +99,7 @@ export function getChildCountIndicatorHtml(item, options) {
     const minCount = options && options.minCount ? options.minCount : 0;
 
     if (item.ChildCount && item.ChildCount > minCount) {
-        return '<div class="countIndicator indicator">' + item.ChildCount + '</div>';
+        return '<div class="countIndicator indicator">' + item.ChildCount.toLocaleString() + '</div>';
     }
 
     return '';
