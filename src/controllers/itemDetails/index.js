5489f34b9237cbc134b2d98595c4ceb7afcc2351
Niels van Velzen <git@ndat.nl>
2022-07-26 22:16:06 +0200

    Add title and filename to download request in item details

--- src/controllers/itemDetails/index.js
+++ src/controllers/itemDetails/index.js
@@ -1966,7 +1966,9 @@ export default function (view, params) {
         download([{
             url: downloadHref,
             itemId: currentItem.Id,
-            serverId: currentItem.serverId
+            serverId: currentItem.serverId,
+            title: currentItem.Name,
+            filename: currentItem.Path.replace(/^.*[\\/]/, '')
         }]);
     }
 
