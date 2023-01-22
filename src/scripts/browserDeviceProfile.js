813845d54b567b90fc55d0d383be0b77354a84a3
nyanmisaka <nst799610810@gmail.com>
2023-01-22 14:07:07 -0500

    Backport pull request #4182 from jellyfin/release-10.8.z
    Fix the progressive mp4 transcoding profile
    
    Original-merge: 5bfffd6209ca4d6c5fe957253d6d7a5117f333ee
    
    Merged-by: Bill Thornton <thornbill@users.noreply.github.com>
    
    Backported-by: Joshua M. Boniface <joshua@boniface.me>

--- src/scripts/browserDeviceProfile.js
+++ src/scripts/browserDeviceProfile.js
@@ -534,7 +534,6 @@ import browser from './browser';
         }
 
         if (canPlayVp8) {
-            mp4VideoCodecs.push('vp8');
             webmVideoCodecs.push('vp8');
         }
 
@@ -717,17 +716,15 @@ import browser from './browser';
             }
         }
 
-        if (webmAudioCodecs.length && webmVideoCodecs.length) {
+        // Progressive mp4 transcoding
+        if (mp4VideoCodecs.length && videoAudioCodecs.length) {
             profile.TranscodingProfiles.push({
-                Container: 'webm',
+                Container: 'mp4',
                 Type: 'Video',
-                AudioCodec: webmAudioCodecs.join(','),
-                // TODO: Remove workaround when servers migrate away from 'vpx' for transcoding profiles.
-                VideoCodec: (canPlayVp8 ? webmVideoCodecs.concat('vpx') : webmVideoCodecs).join(','),
+                AudioCodec: videoAudioCodecs.join(','),
+                VideoCodec: mp4VideoCodecs.join(','),
                 Context: 'Streaming',
                 Protocol: 'http',
-                // If audio transcoding is needed, limit channels to number of physical audio channels
-                // Trying to transcode to 5 channels when there are only 2 speakers generally does not sound good
                 MaxAudioChannels: physicalAudioChannels.toString()
             });
         }
