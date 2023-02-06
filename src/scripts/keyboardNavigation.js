jellyfin/jellyfin-web

commit 36fce00270230dabacb03b757144afb149e37805

Fix keyboard navigation for INPUT and TEXTAREA

src/scripts/keyboardNavigation.js
@@ -44,6 +44,11 @@ const KeyNames = {
  */
 const NavigationKeys = ['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown'];
 
+/**
+ * Elements for which navigation should be constrained.
+ */
+const InteractiveElements = ['INPUT', 'TEXTAREA'];
+
 let hasFieldKey = false;
 try {
     hasFieldKey = 'key' in new KeyboardEvent('keydown');
@@ -91,13 +96,21 @@ export function enable() {
 
         switch (key) {
             case 'ArrowLeft':
-                inputManager.handleCommand('left');
+                if (!InteractiveElements.includes(document.activeElement?.tagName)) {
+                    inputManager.handleCommand('left');
+                } else {
+                    capture = false;
+                }
                 break;
             case 'ArrowUp':
                 inputManager.handleCommand('up');
                 break;
             case 'ArrowRight':
-                inputManager.handleCommand('right');
+                if (!InteractiveElements.includes(document.activeElement?.tagName)) {
+                    inputManager.handleCommand('right');
+                } else {
+                    capture = false;
+                }
                 break;
             case 'ArrowDown':
                 inputManager.handleCommand('down');

commit e580f0c8695f4e28873b3ecc494178b0718cbb61

Fix navigation for some types of INPUT
Regression from 36fce00270230dabacb03b757144afb149e37805

src/scripts/keyboardNavigation.js
@@ -50,6 +50,11 @@ const NavigationKeys = ['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown'];
  */
 const InteractiveElements = ['INPUT', 'TEXTAREA'];
 
+/**
+ * Types of INPUT element for which navigation shouldn't be constrained.
+ */
+const NonInteractiveInputElements = ['button', 'checkbox', 'color', 'file', 'hidden', 'image', 'radio', 'reset', 'submit'];
+
 let hasFieldKey = false;
 try {
     hasFieldKey = 'key' in new KeyboardEvent('keydown');
@@ -84,6 +89,24 @@ export function isNavigationKey(key) {
     return NavigationKeys.indexOf(key) != -1;
 }
 
+/**
+ * Returns _true_ if the element is interactive.
+ *
+ * @param {Element} element - Element.
+ * @return {boolean} _true_ if the element is interactive.
+ */
+export function isInteractiveElement(element) {
+    if (element && InteractiveElements.includes(element.tagName)) {
+        if (element.tagName === 'INPUT') {
+            return !NonInteractiveInputElements.includes(element.type);
+        }
+
+        return true;
+    }
+
+    return false;
+}
+
 export function enable() {
     window.addEventListener('keydown', function (e) {
         const key = getKeyName(e);
@@ -97,7 +120,7 @@ export function enable() {
 
         switch (key) {
             case 'ArrowLeft':
-                if (!InteractiveElements.includes(document.activeElement?.tagName)) {
+                if (!isInteractiveElement(document.activeElement)) {
                     inputManager.handleCommand('left');
                 } else {
                     capture = false;
@@ -107,7 +130,7 @@ export function enable() {
                 inputManager.handleCommand('up');
                 break;
             case 'ArrowRight':
-                if (!InteractiveElements.includes(document.activeElement?.tagName)) {
+                if (!isInteractiveElement(document.activeElement)) {
                     inputManager.handleCommand('right');
                 } else {
                     capture = false;