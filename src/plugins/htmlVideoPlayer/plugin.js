1040685c1ea026940b320381d03441fd58a83e25
Dmitry Lyzo <ashephard0@gmail.com>
2023-02-12 19:25:15 +0300

    Fix subtitle offset reset when seeking progressive stream

--- src/plugins/htmlVideoPlayer/plugin.js
+++ src/plugins/htmlVideoPlayer/plugin.js
@@ -365,7 +365,7 @@ function tryRemoveElement(elem) {
 
             this.#currentTime = null;
 
-            this.resetSubtitleOffset();
+            if (options.resetSubtitleOffset !== false) this.resetSubtitleOffset();
 
             return this.createMediaElement(options).then(elem => {
                 return this.updateVideoUrl(options).then(() => {
