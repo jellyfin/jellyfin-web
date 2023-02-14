62d0354fe09df348c665c813dffb273de4542929
Dmitry Lyzo <ashephard0@gmail.com>
2023-02-14 23:01:00 +0300

    Simplify adding modules to babel-loader

--- webpack.common.js
+++ webpack.common.js
@@ -96,7 +96,17 @@ module.exports = {
             },
             {
                 test: /\.(js|jsx)$/,
-                exclude: /node_modules[\\/](?!@uupaa[\\/]dynamic-import-polyfill|blurhash|date-fns|epubjs|flv.js|libarchive.js|marked|screenfull)/,
+                include: [
+                    path.resolve(__dirname, 'node_modules/@uupaa/dynamic-import-polyfill'),
+                    path.resolve(__dirname, 'node_modules/blurhash'),
+                    path.resolve(__dirname, 'node_modules/date-fns'),
+                    path.resolve(__dirname, 'node_modules/epubjs'),
+                    path.resolve(__dirname, 'node_modules/flv.js'),
+                    path.resolve(__dirname, 'node_modules/libarchive.js'),
+                    path.resolve(__dirname, 'node_modules/marked'),
+                    path.resolve(__dirname, 'node_modules/screenfull'),
+                    path.resolve(__dirname, 'src')
+                ],
                 use: [{
                     loader: 'babel-loader'
                 }]
@@ -118,7 +128,11 @@ module.exports = {
             },
             /* modules that Babel breaks when transforming to ESM */
             {
-                test: /node_modules[\\/](pdfjs-dist|xmldom)[\\/].*\.js$/,
+                test: /\.js$/,
+                include: [
+                    path.resolve(__dirname, 'node_modules/pdfjs-dist'),
+                    path.resolve(__dirname, 'node_modules/xmldom')
+                ],
                 use: [{
                     loader: 'babel-loader',
                     options: {
