586f3bf67f28f04079fceb7d6e44d47c49669b5a
bugfixin <steve@bugfix.in>
2023-02-17 10:54:34 -0800

    Show all Cast&Crew, not just Actors

--- src/controllers/itemDetails/index.js
+++ src/controllers/itemDetails/index.js
@@ -1842,9 +1842,7 @@ function renderSpecials(page, item, user) {
 }
 
 function renderCast(page, item) {
-    const people = (item.People || []).filter(function (p) {
-        return p.Type === 'Actor';
-    });
+    const people = item.People || [];
 
     if (!people.length) {
         page.querySelector('#castCollapsible').classList.add('hide');
