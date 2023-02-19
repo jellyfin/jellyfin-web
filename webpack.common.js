881a50c57645e196f376df21635eb5ecaf786790
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2023-02-19 18:14:09 +0100

    fix: babelify rvfc-polyfill

--- webpack.common.js
+++ webpack.common.js
@@ -146,6 +146,7 @@ const config = {
                 test: /\.(js|jsx)$/,
                 include: [
                     path.resolve(__dirname, 'node_modules/jassub'),
+                    path.resolve(__dirname, 'node_modules/rvfc-polyfill'),
                     path.resolve(__dirname, 'node_modules/@jellyfin/sdk'),
                     path.resolve(__dirname, 'node_modules/@remix-run/router'),
                     path.resolve(__dirname, 'node_modules/@uupaa/dynamic-import-polyfill'),
