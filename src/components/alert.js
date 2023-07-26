24fad10f362319a004cf26e718a4d9d3546539df
Yasin Silavi <ys.silavi@gmail.com>
2023-07-26 10:27:40 +0330

    Format alert module

--- src/components/alert.js
+++ src/components/alert.js
@@ -1,23 +1,25 @@
-import { appRouter } from './router/appRouter';
-import browser from '../scripts/browser';
-import dialog from './dialog/dialog';
-import globalize from '../scripts/globalize';
+import { appRouter } from "./router/appRouter";
+import browser from "../scripts/browser";
+import dialog from "./dialog/dialog";
+import globalize from "../scripts/globalize";
 
 function useNativeAlert() {
     // webOS seems to block modals
     // Tizen 2.x seems to block modals
-    return !browser.web0s
-            && !(browser.tizenVersion && browser.tizenVersion < 3)
-            && browser.tv
-            && window.alert;
+    return (
+        !browser.web0s &&
+        !(browser.tizenVersion && browser.tizenVersion < 3) &&
+        browser.tv &&
+        window.alert
+    );
 }
 
 export default async function (text, title) {
     let options;
-    if (typeof text === 'string') {
+    if (typeof text === "string") {
         options = {
             title: title,
-            text: text
+            text: text,
         };
     } else {
         options = text;
@@ -26,15 +28,15 @@ export default async function (text, title) {
     await appRouter.ready();
 
     if (useNativeAlert()) {
-        alert((options.text || '').replaceAll('<br/>', '\n'));
+        alert((options.text || "").replaceAll("<br/>", "\n"));
         return Promise.resolve();
     } else {
         const items = [];
 
         items.push({
-            name: globalize.translate('ButtonGotIt'),
-            id: 'ok',
-            type: 'submit'
+            name: globalize.translate("ButtonGotIt"),
+            id: "ok",
+            type: "submit",
         });
 
         options.buttons = items;
