6664b6ec62fac3ab89374c3ae2431b714f795cf1
Yasin Silavi <ys.silavi@gmail.com>
2023-09-10 23:20:20 +0330

    style: use singlequote for strings

--- src/components/alert.js
+++ src/components/alert.js
@@ -1,33 +1,33 @@
-import { appRouter } from "./router/appRouter";
-import browser from "../scripts/browser";
-import dialog from "./dialog/dialog";
-import globalize from "../scripts/globalize";
+import { appRouter } from './router/appRouter';
+import browser from '../scripts/browser';
+import dialog from './dialog/dialog';
+import globalize from '../scripts/globalize';
 
 export default async function (text, title) {
     // Modals seem to be blocked on Web OS and Tizen 2.x
     const canUseNativeAlert = !!(
-        !browser.web0s &&
-        !(browser.tizenVersion && browser.tizenVersion < 3) &&
-        browser.tv &&
-        window.alert
+        !browser.web0s
+        && !(browser.tizenVersion && browser.tizenVersion < 3)
+        && browser.tv
+        && window.alert
     );
 
-    let options = typeof text === "string" ? { title, text } : text;
+    const options = typeof text === 'string' ? { title, text } : text;
 
     await appRouter.ready();
 
     if (canUseNativeAlert) {
-        alert((options.text || "").replaceAll("<br/>", "\n"));
+        alert((options.text || '').replaceAll('<br/>', '\n'));
 
         return Promise.resolve();
     }
 
     const items = [
         {
-            name: globalize.translate("ButtonGotIt"),
-            id: "ok",
-            type: "submit",
-        },
+            name: globalize.translate('ButtonGotIt'),
+            id: 'ok',
+            type: 'submit'
+        }
     ];
 
     options.buttons = items;
