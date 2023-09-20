399157b08d526cd17472f17459d12a8c41a2190d
Bill Thornton <thornbill@users.noreply.github.com>
2023-09-20 00:04:08 -0400

    Add search params to redirects

--- src/components/router/Redirect.tsx
+++ src/components/router/Redirect.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import { Navigate, Route } from 'react-router-dom';
+import { Navigate, Route, useLocation } from 'react-router-dom';
 
 export interface Redirect {
     from: string
     to: string
 }
 
+const RedirectWithSearch = ({ to }: { to: string }) => {
+    const { search } = useLocation();
+
+    return (
+        <Navigate
+            replace
+            to={`${to}${search}`}
+        />
+    );
+};
+
 export function toRedirectRoute({ from, to }: Redirect) {
     return (
         <Route
             key={from}
             path={from}
-            element={<Navigate replace to={to} />}
+            element={<RedirectWithSearch to={to} />}
         />
     );
 }
