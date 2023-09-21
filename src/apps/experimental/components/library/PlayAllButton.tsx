bc23c4aa351cd444d989d7a4ba0bf989fa591c22
grafixeyehero <grafixeyehero@yahoo.com>
2023-09-21 19:43:55 +0300

    pass queryOptions

--- src/apps/experimental/components/library/PlayAllButton.tsx
+++ src/apps/experimental/components/library/PlayAllButton.tsx
@@ -5,25 +5,43 @@ import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 
 import { playbackManager } from 'components/playback/playbackmanager';
 import globalize from 'scripts/globalize';
+import { getFiltersQuery } from 'utils/items';
+import { LibraryViewSettings } from 'types/library';
+import { LibraryTab } from 'types/libraryTab';
 
 interface PlayAllButtonProps {
     item: BaseItemDto | undefined;
     items: BaseItemDto[];
+    viewType: LibraryTab;
     hasFilters: boolean;
+    libraryViewSettings: LibraryViewSettings
 }
 
-const PlayAllButton: FC<PlayAllButtonProps> = ({ item, items, hasFilters }) => {
+const PlayAllButton: FC<PlayAllButtonProps> = ({ item, items, viewType, hasFilters, libraryViewSettings }) => {
     const play = useCallback(() => {
         if (item && !hasFilters) {
             playbackManager.play({
-                items: [item]
+                items: [item],
+                autoplay: true,
+                queryOptions: {
+                    SortBy: [libraryViewSettings.SortBy],
+                    SortOrder: [libraryViewSettings.SortOrder]
+                }
             });
         } else {
             playbackManager.play({
-                items: items
+                items: items,
+                autoplay: true,
+                queryOptions: {
+                    ParentId: item?.Id ?? undefined,
+                    ...getFiltersQuery(viewType, libraryViewSettings),
+                    SortBy: [libraryViewSettings.SortBy],
+                    SortOrder: [libraryViewSettings.SortOrder]
+                }
+
             });
         }
-    }, [hasFilters, item, items]);
+    }, [hasFilters, item, items, libraryViewSettings, viewType]);
 
     return (
         <IconButton
