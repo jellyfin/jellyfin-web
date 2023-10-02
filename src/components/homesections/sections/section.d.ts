8149076fc54c124219091070674e5dfff6add622
Bill Thornton <thornbill@users.noreply.github.com>
2023-10-02 13:43:06 -0400

    Fix function return type

--- src/components/homesections/sections/section.d.ts
+++ src/components/homesections/sections/section.d.ts
@@ -1,11 +1,12 @@
 import type { BaseItemDto } from '@jellyfin/sdk/lib/generated-client/models/base-item-dto';
+import type { BaseItemDtoQueryResult } from '@jellyfin/sdk/lib/generated-client/models/base-item-dto-query-result';
 
 export interface SectionOptions {
     enableOverflow: boolean
 }
 
 export type SectionContainerElement = {
-    fetchData: () => void
+    fetchData: () => Promise<BaseItemDtoQueryResult | BaseItemDto[]>
     getItemsHtml: (items: BaseItemDto[]) => void
     parentContainer: HTMLElement
 } & Element;
