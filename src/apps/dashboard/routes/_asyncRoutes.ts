62f9e7581acf445acf6a2aac4158ada2158c0aac
Bill Thornton <thornbill@users.noreply.github.com>
2023-10-03 13:15:52 -0400

    Fix add user route

--- src/apps/dashboard/routes/_asyncRoutes.ts
+++ src/apps/dashboard/routes/_asyncRoutes.ts
@@ -3,10 +3,10 @@ import type { AsyncRoute } from 'components/router/AsyncRoute';
 export const ASYNC_ADMIN_ROUTES: AsyncRoute[] = [
     { path: 'activity' },
     { path: 'notifications' },
-    { path: 'users/new' },
     { path: 'users' },
-    { path: 'users/profile' },
     { path: 'users/access' },
+    { path: 'users/add' },
     { path: 'users/parentalcontrol' },
-    { path: 'users/password' }
+    { path: 'users/password' },
+    { path: 'users/profile' }
 ];
