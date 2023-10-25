a05c6e4d5801c822de289809efc7e3daaee5da70
grafixeyehero <grafixeyehero@yahoo.com>
2023-10-26 02:05:38 +0300

    Add tv show view

--- src/apps/experimental/routes/asyncRoutes/user.ts
+++ src/apps/experimental/routes/asyncRoutes/user.ts
@@ -5,5 +5,6 @@ export const ASYNC_USER_ROUTES: AsyncRoute[] = [
     { path: 'search.html', page: 'search' },
     { path: 'userprofile.html', page: 'user/userprofile' },
     { path: 'home.html', page: 'home', type: AsyncRouteType.Experimental },
-    { path: 'movies.html', page: 'movies', type: AsyncRouteType.Experimental }
+    { path: 'movies.html', page: 'movies', type: AsyncRouteType.Experimental },
+    { path: 'tv.html', page: 'shows', type: AsyncRouteType.Experimental }
 ];
