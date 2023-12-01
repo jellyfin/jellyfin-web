41502f80723bc762f6b62d593a1ff3bb36ccc505
asm9 <asm9@iu.edu>
2023-11-30 21:00:53 -0500

    fix eslint errors in dashboard.js

--- src/utils/dashboard.js
+++ src/utils/dashboard.js
@@ -176,16 +176,16 @@ export function capabilities(appHost) {
     return {
         PlayableMediaTypes: ['Audio', 'Video'],
         SupportedCommands: ['MoveUp', 'MoveDown', 'MoveLeft', 'MoveRight',
-        'PageUp', 'PageDown', 'PreviousLetter', 'NextLetter', 'ToggleOsd', 
-        'ToggleContextMenu', 'Select', 'Back', 'SendKey', 'SendString', 
-        'GoHome', 'GoToSettings', 'VolumeUp', 'VolumeDown', 'Mute', 'Unmute', 
-        'ToggleMute', 'SetVolume', 'SetAudioStreamIndex', 'SetSubtitleStreamIndex', 
-        'DisplayContent', 'GoToSearch', 'DisplayMessage', 'SetRepeatMode', 
-        'SetShuffleQueue', 'ChannelUp', 'ChannelDown', 'PlayMediaSource', 'PlayTrailers'],
+            'PageUp', 'PageDown', 'PreviousLetter', 'NextLetter', 'ToggleOsd',
+            'ToggleContextMenu', 'Select', 'Back', 'SendKey', 'SendString',
+            'GoHome', 'GoToSettings', 'VolumeUp', 'VolumeDown', 'Mute', 'Unmute',
+            'ToggleMute', 'SetVolume', 'SetAudioStreamIndex', 'SetSubtitleStreamIndex',
+            'DisplayContent', 'GoToSearch', 'DisplayMessage', 'SetRepeatMode',
+            'SetShuffleQueue', 'ChannelUp', 'ChannelDown', 'PlayMediaSource', 'PlayTrailers'],
         SupportsPersistentIdentifier: window.appMode === 'cordova' || window.appMode === 'android',
         SupportsMediaControl: true,
         ...appHost.getPushTokenInfo()
-  };
+    };
 }
 
 export function selectServer() {
