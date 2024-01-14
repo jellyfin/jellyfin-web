7d3bde3617cfa3e350894d5192ad78bbb15d41e2
Damian Kacperski <7dami77@gmail.com>
2024-01-14 19:44:00 +0100

    Move PlaybackCommandButton from remotecontrol to separate component

---
+++ src/components/remotecontrol/playbackCommandButton.ts
@@ -0,0 +1,56 @@
+import { playbackManager } from 'components/playback/playbackmanager';
+import { PlayerPlugin } from 'types/player';
+
+export default class PlaybackCommandButton {
+    currentPlayer: PlayerPlugin|null = null;
+    btnCommand: HTMLButtonElement;
+
+    constructor (context: HTMLButtonElement) {
+        this.btnCommand = context;
+        this.btnCommand.addEventListener('click', this.onBtnCommandClick);
+    }
+
+    private onBtnCommandClick = () => {
+        if (this.currentPlayer) {
+            playbackManager.sendCommand({
+                Name: this.btnCommand.getAttribute('data-command')
+            }, this.currentPlayer);
+        }
+    };
+
+    onShow(player: PlayerPlugin|null) {
+        this.bindToPlayer(player);
+    }
+
+    destroy() {
+        this.releaseCurrentPlayer();
+    }
+
+    onPlayerChange(player: PlayerPlugin|null) {
+        this.bindToPlayer(player);
+    }
+
+    private bindToPlayer(player: PlayerPlugin|null) {
+        this.releaseCurrentPlayer();
+        this.currentPlayer = player;
+        if (player) {
+            const playerInfo = playbackManager.getPlayerInfo();
+            const supportedCommands = playerInfo?.supportedCommands || [];
+            this.updateSupportedCommands(supportedCommands);
+        }
+    }
+
+    private releaseCurrentPlayer() {
+        const player = this.currentPlayer;
+
+        if (player) {
+            this.currentPlayer = null;
+        }
+    }
+
+    private updateSupportedCommands(commands: string[]) {
+        const command = this.btnCommand.getAttribute('data-command');
+        const enableButton = command ? commands.indexOf(command) !== -1 : false;
+        this.btnCommand.disabled = !enableButton;
+    }
+}
