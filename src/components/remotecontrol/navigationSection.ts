c6736b2ad40bf34accdcbcda7be3459354752611
Damian Kacperski <7dami77@gmail.com>
2024-01-15 00:23:59 +0100

    Move NavigationSection from remotecontrol to separate component

---
+++ src/components/remotecontrol/navigationSection.ts
@@ -0,0 +1,41 @@
+import { PlayerPlugin } from 'types/player';
+import PlaybackCommandButton from './playbackCommandButton';
+
+export default class NavigationSection {
+    playbackCommandButtons: PlaybackCommandButton[];
+
+    constructor (context: HTMLElement) {
+        this.playbackCommandButtons = [];
+        const navigationSection = context.querySelector('.navigationSection');
+        const buttons = navigationSection?.querySelectorAll<HTMLButtonElement>('.btnCommand') ?? [];
+        for (const button of buttons) {
+            this.playbackCommandButtons.push(new PlaybackCommandButton(button));
+        }
+    }
+
+    onShow(player: PlayerPlugin|null) {
+        this.playbackCommandButtons.forEach((playbackCommandButton) => {
+            playbackCommandButton.onShow(player);
+        });
+    }
+
+    destroy() {
+        this.playbackCommandButtons.forEach((playbackCommandButton) => {
+            playbackCommandButton.destroy();
+        });
+    }
+
+    updatePlayerState(context: HTMLElement, supportedCommands: string[], player: PlayerPlugin|null) {
+        if (supportedCommands.includes('Select') && player && !player.isLocalPlayer) {
+            context.querySelector('.navigationSection')?.classList.remove('hide');
+        } else {
+            context.querySelector('.navigationSection')?.classList.add('hide');
+        }
+    }
+
+    onPlayerChange(player: PlayerPlugin|null) {
+        this.playbackCommandButtons.forEach((playbackCommandButton) => {
+            playbackCommandButton.onPlayerChange(player);
+        });
+    }
+}
