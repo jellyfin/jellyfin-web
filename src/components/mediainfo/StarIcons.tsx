c3b5d503131572f43d28c5b45b5cd76ddf14c450
grafixeyehero <grafixeyehero@yahoo.com>
2024-01-31 03:01:58 +0300

    Convert mediainfo PrimaryMediaInfo to react

---
+++ src/components/mediainfo/StarIcons.tsx
@@ -0,0 +1,29 @@
+import React, { FC } from 'react';
+import classNames from 'classnames';
+import StarIcon from '@mui/icons-material/Star';
+import Box from '@mui/material/Box';
+
+interface StarIconsProps {
+    className?: string;
+    communityRating: number;
+}
+
+const StarIcons: FC<StarIconsProps> = ({ className, communityRating }) => {
+    const cssClass = classNames(
+        'mediaInfoItem',
+        'mediaInfoText',
+        'starRatingContainer',
+        className
+    );
+
+    return (
+        <Box className={cssClass}>
+            <StarIcon fontSize={'small'} sx={{
+                color: '#f2b01e'
+            }} />
+            {communityRating.toFixed(1)}
+        </Box>
+    );
+};
+
+export default StarIcons;
