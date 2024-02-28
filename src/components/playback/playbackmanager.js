d68ef36465dbaf18a02499ddebc6cf0d2f4b7887
Dmitry Lyzo <ashephard0@gmail.com>
2024-02-28 19:28:32 +0300

    Use stream compatibility flag
    Keep old logic for backward compatibility.

--- src/components/playback/playbackmanager.js
+++ src/components/playback/playbackmanager.js
@@ -1300,6 +1300,10 @@ class PlaybackManager {
                 return false;
             }
 
+            if (mediaStream.SupportsDirectPlay != null) {
+                return mediaStream.SupportsDirectPlay;
+            }
+
             const container = mediaSource.Container.toLowerCase();
             const codec = (mediaStream.Codec || '').toLowerCase();
 
