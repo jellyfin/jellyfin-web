ec1f016d5b2a1e92b09047b742888a9a7eadaa84
Bill Thornton <thornbill@users.noreply.github.com>
2024-03-04 13:49:29 -0500

    Update notifications page ui

--- src/apps/dashboard/routes/notifications.tsx
+++ src/apps/dashboard/routes/notifications.tsx
@@ -1,23 +1,13 @@
+import Alert from '@mui/material/Alert/Alert';
+import Box from '@mui/material/Box/Box';
+import Button from '@mui/material/Button/Button';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import Page from 'components/Page';
 import globalize from 'scripts/globalize';
 
-const PluginLink = () => (
-    <div
-        dangerouslySetInnerHTML={{
-            __html: `<a
-                is='emby-linkbutton'
-                class='button-link'
-                href='#/dashboard/plugins/add?name=Webhook&guid=71552a5a5c5c4350a2aeebe451a30173'
-            >
-                ${globalize.translate('GetThePlugin')}
-            </a>`
-        }}
-    />
-);
-
-const Notifications = () => (
+const NotificationsPage = () => (
     <Page
         id='notificationSettingPage'
         title={globalize.translate('Notifications')}
@@ -25,12 +15,20 @@ const Notifications = () => (
     >
         <div className='content-primary'>
             <h2>{globalize.translate('Notifications')}</h2>
-            <p>
-                {globalize.translate('NotificationsMovedMessage')}
-            </p>
-            <PluginLink />
+
+            <Alert severity='info'>
+                <Box sx={{ marginBottom: 2 }}>
+                    {globalize.translate('NotificationsMovedMessage')}
+                </Box>
+                <Button
+                    component={Link}
+                    to='/dashboard/plugins/add?name=Webhook&guid=71552a5a5c5c4350a2aeebe451a30173'
+                >
+                    {globalize.translate('GetThePlugin')}
+                </Button>
+            </Alert>
         </div>
     </Page>
 );
 
-export default Notifications;
+export default NotificationsPage;
