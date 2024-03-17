b37eeba62af478347b6bb20503e4b7a0652f055a
grafixeyehero <grafixeyehero@yahoo.com>
2024-03-17 23:41:22 +0300

    Add User Edit page

--- src/apps/dashboard/routes/_asyncRoutes.ts
+++ src/apps/dashboard/routes/_asyncRoutes.ts
@@ -7,6 +7,7 @@ export const ASYNC_ADMIN_ROUTES: AsyncRoute[] = [
     { path: 'users', type: AsyncRouteType.Dashboard },
     { path: 'users/access', type: AsyncRouteType.Dashboard },
     { path: 'users/add', type: AsyncRouteType.Dashboard },
+    { path: 'users/edit', type: AsyncRouteType.Dashboard },
     { path: 'users/parentalcontrol', type: AsyncRouteType.Dashboard },
     { path: 'users/password', type: AsyncRouteType.Dashboard },
     { path: 'users/profile', type: AsyncRouteType.Dashboard }
