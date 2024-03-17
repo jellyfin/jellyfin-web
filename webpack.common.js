8125c68d2450290f46ee3b8967f0ed87861660bb
Dmitry Lyzo <ashephard0@gmail.com>
2024-03-17 17:18:23 +0300

    Migrate back from JASSUB to JavascriptSubtitlesOctopus
    JASSUB has some problems with non-Latin languages.
    
    This reverts:
    3560715f410354a3c65237b85062812a53903181
    0b3d2d841fc9d03af9faf4b9aef2eec044e3daab
    93c4dbbe4cce4c1c3f3c678abc77d3b0ffa0e874
    478f81fecf4e38b0456c99978df11c8bd9b9c224
    f0e8eb52bbea3f6941020765aa1e2b539996508c
    52c8cffc827a2532b161029f3df614e3db1a0fad

--- webpack.common.js
+++ webpack.common.js
@@ -10,11 +10,13 @@ const packageJson = require('./package.json');
 const Assets = [
     'native-promise-only/npo.js',
     'libarchive.js/dist/worker-bundle.js',
+    '@jellyfin/libass-wasm/dist/js/subtitles-octopus-worker.js',
+    '@jellyfin/libass-wasm/dist/js/subtitles-octopus-worker.data',
+    '@jellyfin/libass-wasm/dist/js/subtitles-octopus-worker.wasm',
+    '@jellyfin/libass-wasm/dist/js/subtitles-octopus-worker-legacy.js',
+    '@jellyfin/libass-wasm/dist/js/subtitles-octopus-worker-legacy.data',
+    '@jellyfin/libass-wasm/dist/js/subtitles-octopus-worker-legacy.js.mem',
     'pdfjs-dist/build/pdf.worker.js'
-];
-
-const JassubWasm = [
-    'jassub/dist/default.woff2'
 ];
 
 const LibarchiveWasm = [
@@ -102,14 +104,6 @@ const config = {
                 };
             })
         }),
-        new CopyPlugin({
-            patterns: JassubWasm.map(asset => {
-                return {
-                    from: path.resolve(__dirname, `./node_modules/${asset}`),
-                    to: path.resolve(__dirname, './dist')
-                };
-            })
-        }),
         new ForkTsCheckerWebpackPlugin({
             typescript: {
                 configFile: path.resolve(__dirname, 'tsconfig.json')
@@ -182,8 +176,7 @@ const config = {
             {
                 test: /\.(js|jsx|mjs)$/,
                 include: [
-                    path.resolve(__dirname, 'node_modules/event-target-polyfill'),
-                    path.resolve(__dirname, 'node_modules/rvfc-polyfill'),
+                    path.resolve(__dirname, 'node_modules/@jellyfin/libass-wasm'),
                     path.resolve(__dirname, 'node_modules/@jellyfin/sdk'),
                     path.resolve(__dirname, 'node_modules/@react-hook/latest'),
                     path.resolve(__dirname, 'node_modules/@react-hook/passive-layout-effect'),
@@ -221,20 +214,6 @@ const config = {
                     options: {
                         cacheCompression: false,
                         cacheDirectory: true
-                    }
-                }]
-            },
-            {
-                test: /\.js$/,
-                include: [
-                    path.resolve(__dirname, 'node_modules/jassub')
-                ],
-                use: [{
-                    loader: 'babel-loader',
-                    options: {
-                        cacheCompression: false,
-                        cacheDirectory: true,
-                        presets: ['@babel/preset-env']
                     }
                 }]
             },
