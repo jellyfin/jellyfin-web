f3f652e9b7891b4ae64e675aa36dab5611489936
Airthee <13355624+Airthee@users.noreply.github.com>
2024-04-23 21:48:38 +0100

    fix lint

--- src/utils/card.test.ts
+++ src/utils/card.test.ts
@@ -6,32 +6,32 @@ describe('Utils: card', () => {
         it('Should return "overflowSquare"', () => {
             const result = card.getSquareShape(true);
             expect(result).toEqual('overflowSquare');
-        })
+        });
         it('Should return "square"', () => {
             const result = card.getSquareShape(false);
             expect(result).toEqual('square');
-        })
-    })
+        });
+    });
 
     describe('Function: getBackdropShape', () => {
         it('Should return "overflowBackdrop"', () => {
             const result = card.getBackdropShape(true);
             expect(result).toEqual('overflowBackdrop');
-        })
+        });
         it('Should return "backdrop"', () => {
             const result = card.getBackdropShape(false);
             expect(result).toEqual('backdrop');
-        })
-    })
+        });
+    });
 
     describe('Function: getPortraitShape', () => {
         it('Should return "overflowPortrait"', () => {
             const result = card.getPortraitShape(true);
             expect(result).toEqual('overflowPortrait');
-        })
+        });
         it('Should return "portrait"', () => {
             const result = card.getPortraitShape(false);
             expect(result).toEqual('portrait');
-        })
-    })
-})
+        });
+    });
+});
--- src/utils/dateFnsLocale.test.ts
+++ src/utils/dateFnsLocale.test.ts
@@ -6,8 +6,8 @@ describe('Utils: dateFnsLocale', () => {
         it('Should return "en-US" by default', () => {
             const { code } = dateFnsLocale.getLocale();
             expect(code).toEqual('en-US');
-        })
-    })
+        });
+    });
 
     describe('Function: getLocaleWithSuffix', () => {
         it('Should return "en-US" by default with addSuffix to true', () => {
@@ -15,8 +15,8 @@ describe('Utils: dateFnsLocale', () => {
 
             expect(addSuffix).toEqual(true);
             expect(locale.code).toEqual('en-US');
-        })
-    })
+        });
+    });
 
     describe('Function: updateLocale', () => {
         it('Should import "fr-ca" locale', async () => {
@@ -24,32 +24,35 @@ describe('Utils: dateFnsLocale', () => {
 
             await dateFnsLocale.updateLocale('fr-ca');
             const { code } = dateFnsLocale.getLocale();
-            const { locale: localeWithSuffix } = dateFnsLocale.getLocaleWithSuffix();
+            const { locale: localeWithSuffix } =
+                dateFnsLocale.getLocaleWithSuffix();
 
             expect(code).toEqual(expectedCode);
             expect(localeWithSuffix.code).toEqual(expectedCode);
-        })
+        });
 
         it('Should import "fr" locale', async () => {
             const expectedCode = 'fr';
 
             await dateFnsLocale.updateLocale('fr-fr');
             const { code } = dateFnsLocale.getLocale();
-            const { locale: localeWithSuffix } = dateFnsLocale.getLocaleWithSuffix();
+            const { locale: localeWithSuffix } =
+                dateFnsLocale.getLocaleWithSuffix();
 
             expect(code).toEqual(expectedCode);
             expect(localeWithSuffix.code).toEqual(expectedCode);
-        })
+        });
 
         it('Should import "en-US" locale if given locale is not found', async () => {
             const expectedCode = 'en-US';
 
             await dateFnsLocale.updateLocale('unknown-unknown');
             const { code } = dateFnsLocale.getLocale();
-            const { locale: localeWithSuffix } = dateFnsLocale.getLocaleWithSuffix();
+            const { locale: localeWithSuffix } =
+                dateFnsLocale.getLocaleWithSuffix();
 
             expect(code).toEqual(expectedCode);
             expect(localeWithSuffix.code).toEqual(expectedCode);
-        })
-    })
-})
+        });
+    });
+});
