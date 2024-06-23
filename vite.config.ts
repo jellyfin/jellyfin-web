339a7f3140243af180a1c791ba6322eb742c631f
Pithaya <19533412+Pithaya@users.noreply.github.com>
2024-06-23 16:33:56 +0200

    Fix tests failing with absolute paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
     test: {
         environment: 'jsdom',
         globalSetup: 'vitest.setup.ts'
-    }
+    },
+    plugins: [tsconfigPaths()]
 });
