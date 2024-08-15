f8e428789b715fe8b13ef78a61dd4a574bd6ec99
venkata nadha reddy <45334400+venkat-karasani@users.noreply.github.com>
2024-08-15 19:59:56 +0000

    Use production year for movies in video player (#5765)
    * In video player, changes to display production year rather than release year for movies.
    
    * making it consistent

--- src/controllers/playback/video/index.js
+++ src/controllers/playback/video/index.js
@@ -260,7 +260,9 @@ export default function (view) {
 
         // Display the item with its premiere date if it has one
         let title = itemName;
-        if (item.PremiereDate) {
+        if (item.Type == 'Movie' && item.ProductionYear) {
+            title += ` (${datetime.toLocaleString(item.ProductionYear, { useGrouping: false })})`;
+        } else if (item.PremiereDate) {
             try {
                 const year = datetime.toLocaleString(datetime.parseISO8601Date(item.PremiereDate).getFullYear(), { useGrouping: false });
                 title += ` (${year})`;
