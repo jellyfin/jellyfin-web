76bde38e22d1efa4b763764b9241fa6a17f5601d
Bill Thornton <thornbill@users.noreply.github.com>
2024-09-23 12:09:25 -0400

    Remove unused imports

--- src/components/search/SearchSuggestions.tsx
+++ src/components/search/SearchSuggestions.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useEffect, useState } from 'react';
+import React, { FunctionComponent } from 'react';
 
 import Loading from 'components/loading/LoadingComponent';
 import { appRouter } from '../router/appRouter';
