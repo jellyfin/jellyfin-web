5b5d1cb2551720612bc492edee0c1637aeb682da
Ethan <ethanarns95@gmail.com>
2024-10-05 14:01:31 -0700

    Requested pull request fixes

--- src/scripts/screensavermanager.js
+++ src/scripts/screensavermanager.js
@@ -11,13 +11,7 @@ import './screensavermanager.scss';
 function getMinIdleTime() {
     // Returns the minimum amount of idle time required before the screen saver can be displayed
     //time units used Millisecond
-    const timeSeconds = userSettings.get('screensaverTime', false);
-    // If it is zero or undefined
-    if (!timeSeconds) {
-        // Defaults to 3 minutes
-        return 180000;
-    }
-    return timeSeconds * 1000;
+    return userSettings.get('screensaverTime') * 1000;
 }
 
 let lastFunctionalEvent = 0;
