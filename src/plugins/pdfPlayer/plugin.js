7e4921f1dc59c33be4af99e4e585eb6061dfaec8
Martin <martin@dul.li>
2024-10-13 10:24:08 +0200

    Fix blurry pdf player rendering for all viewport sizes (#6182)

--- src/plugins/pdfPlayer/plugin.js
+++ src/plugins/pdfPlayer/plugin.js
@@ -290,22 +290,16 @@ export class PdfPlayer {
     }
 
     renderPage(canvas, number) {
+        const devicePixelRatio = window.devicePixelRatio || 1;
         this.book.getPage(number).then(page => {
-            const width = dom.getWindowSize().innerWidth;
-            const height = dom.getWindowSize().innerHeight;
-            const scale = Math.ceil(window.devicePixelRatio || 1);
+            const original = page.getViewport({ scale: 1 });
+            const scale = Math.max((window.screen.height / original.height), (window.screen.width / original.width)) * devicePixelRatio;
             const viewport = page.getViewport({ scale });
-            const context = canvas.getContext('2d');
+
             canvas.width = viewport.width;
             canvas.height = viewport.height;
 
-            if (width < height) {
-                canvas.style.width = '100%';
-                canvas.style.height = 'auto';
-            } else {
-                canvas.style.height = '100%';
-                canvas.style.width = 'auto';
-            }
+            const context = canvas.getContext('2d');
 
             const renderContext = {
                 canvasContext: context,
