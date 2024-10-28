acde0685b5510d1f8f358c74fa9e0e57d28fd4be
viown <48097677+viown@users.noreply.github.com>
2024-10-28 17:36:10 +0300

    Use a constant for defaults

--- src/apps/stable/features/playback/utils/mediaSegmentSettings.ts
+++ src/apps/stable/features/playback/utils/mediaSegmentSettings.ts
@@ -5,14 +5,16 @@ import { UserSettings } from 'scripts/settings/userSettings';
 import { MediaSegmentAction } from '../constants/mediaSegmentAction';
 
 const PREFIX = 'segmentTypeAction';
+const DEFAULT_ACTIONS: Partial<Record<MediaSegmentType, MediaSegmentAction>> = {
+    [MediaSegmentType.Intro]: MediaSegmentAction.AskToSkip,
+    [MediaSegmentType.Outro]: MediaSegmentAction.AskToSkip
+};
 
 export const getId = (type: MediaSegmentType) => `${PREFIX}__${type}`;
 
 export function getMediaSegmentAction(userSettings: UserSettings, type: MediaSegmentType): MediaSegmentAction {
     const action = userSettings.get(getId(type), false);
-    let defaultAction = MediaSegmentAction.None;
-    if (type === MediaSegmentType.Intro || type === MediaSegmentType.Outro) {
-        defaultAction = MediaSegmentAction.AskToSkip;
-    }
+    const defaultAction = DEFAULT_ACTIONS[type] || MediaSegmentAction.None;
+
     return action ? action as MediaSegmentAction : defaultAction;
 }
