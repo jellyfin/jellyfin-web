bc7ec0e87628971a0dbbe467d299132fd89f2fba
Bill Thornton <thornbill@users.noreply.github.com>
2024-10-30 16:01:58 -0400

    Remove self cast target

--- src/components/playback/playbackmanager.js
+++ src/components/playback/playbackmanager.js
@@ -853,29 +853,15 @@ export class PlaybackManager {
             const promises = players.filter(displayPlayerIndividually).map(getPlayerTargets);
 
             return Promise.all(promises).then(function (responses) {
-                return ServerConnections.currentApiClient().getCurrentUser().then(function (user) {
-                    const targets = [];
+                const targets = [];
 
-                    targets.push({
-                        name: globalize.translate('HeaderMyDevice'),
-                        id: 'localplayer',
-                        playerName: 'localplayer',
-                        playableMediaTypes: ['Audio', 'Video', 'Photo', 'Book'],
-                        isLocalPlayer: true,
-                        supportedCommands: self.getSupportedCommands({
-                            isLocalPlayer: true
-                        }),
-                        user: user
-                    });
-
-                    for (const subTargets of responses) {
-                        for (const subTarget of subTargets) {
-                            targets.push(subTarget);
-                        }
+                for (const subTargets of responses) {
+                    for (const subTarget of subTargets) {
+                        targets.push(subTarget);
                     }
+                }
 
-                    return targets.sort(sortPlayerTargets);
-                });
+                return targets.sort(sortPlayerTargets);
             });
         };
 
