3fd36ee626848e30b8f8d868d4a36ae00efcafe5
Dmitry Lyzo <ashephard0@gmail.com>
2024-12-01 20:34:36 +0300

    Add element-closest-polyfill

--- src/lib/legacy/index.ts
+++ src/lib/legacy/index.ts
@@ -1,6 +1,7 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import 'jquery';
+import 'element-closest-polyfill';
 import 'fast-text-encoding';
 import 'intersection-observer';
 import 'classlist.js';
