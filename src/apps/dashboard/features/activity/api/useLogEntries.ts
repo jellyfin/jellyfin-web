5eb57c78a07f6c7cdb0c1128cce58664d629cf91
viown <48097677+viown@users.noreply.github.com>
2024-12-17 21:35:45 +0300

    Move data loading to separate hooks

--- src/apps/dashboard/features/activity/api/useLogEntries.ts
+++ src/apps/dashboard/features/activity/api/useLogEntries.ts
@@ -3,7 +3,6 @@ import type { AxiosRequestConfig } from 'axios';
 import type { Api } from '@jellyfin/sdk';
 import { getActivityLogApi } from '@jellyfin/sdk/lib/utils/api/activity-log-api';
 import { useQuery } from '@tanstack/react-query';
-
 import { useApi } from 'hooks/useApi';
 
 const fetchLogEntries = async (
