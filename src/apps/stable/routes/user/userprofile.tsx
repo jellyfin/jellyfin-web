ba03274ce0f7bceb58b15484a5e37c3a43443f7d
viown <48097677+viown@users.noreply.github.com>
2024-12-20 22:36:26 +0300

    Add libraryMenu as a dependency

--- src/apps/stable/routes/user/userprofile.tsx
+++ src/apps/stable/routes/user/userprofile.tsx
@@ -97,7 +97,7 @@ const UserProfile: FunctionComponent = () => {
         }).catch(err => {
             console.error('[userprofile] failed to get current user', err);
         });
-    }, [user]);
+    }, [user, libraryMenu]);
 
     useEffect(() => {
         const page = element.current;
