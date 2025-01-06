e08de5175dbfeb0039edc3777325272f339cfd74
viown <48097677+viown@users.noreply.github.com>
2025-01-06 23:27:38 +0300

    Move to alphabetical order

--- src/apps/dashboard/routes/_asyncRoutes.ts
+++ src/apps/dashboard/routes/_asyncRoutes.ts
@@ -3,9 +3,9 @@ import { AsyncRouteType, type AsyncRoute } from 'components/router/AsyncRoute';
 export const ASYNC_ADMIN_ROUTES: AsyncRoute[] = [
     { path: 'activity', type: AsyncRouteType.Dashboard },
     { path: 'branding', type: AsyncRouteType.Dashboard },
+    { path: 'logs', type: AsyncRouteType.Dashboard },
     { path: 'playback/trickplay', type: AsyncRouteType.Dashboard },
     { path: 'plugins/:pluginId', page: 'plugins/plugin', type: AsyncRouteType.Dashboard },
-    { path: 'logs', type: AsyncRouteType.Dashboard },
     { path: 'users', type: AsyncRouteType.Dashboard },
     { path: 'users/access', type: AsyncRouteType.Dashboard },
     { path: 'users/add', type: AsyncRouteType.Dashboard },
