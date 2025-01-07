1d72f276472768377128472d4c500d637f820847
Kevin G <kevin@myplaceonline.com>
2025-01-07 17:27:13 -0600

    Issue #5486: When changing player types, destroy old player
    Signed-off-by: Kevin G <kevin@myplaceonline.com>

--- src/components/playback/playbackmanager.js
+++ src/components/playback/playbackmanager.js
@@ -3483,7 +3483,7 @@ export class PlaybackManager {
             const nextItemPlayOptions = nextItem ? (nextItem.item.playOptions || getDefaultPlayOptions()) : getDefaultPlayOptions();
             const newPlayer = nextItem ? getPlayer(nextItem.item, nextItemPlayOptions) : null;
 
-            if (newPlayer !== player) {
+            if (!newPlayer) {
                 data.streamInfo = null;
                 destroyPlayer(player);
                 removeCurrentPlayer(player);
@@ -3491,12 +3491,21 @@ export class PlaybackManager {
 
             if (errorOccurred) {
                 showPlaybackInfoErrorMessage(self, 'PlaybackError' + displayErrorCode);
-            } else if (nextItem) {
+            } else if (newPlayer) {
                 const apiClient = ServerConnections.getApiClient(nextItem.item.ServerId);
 
                 apiClient.getCurrentUser().then(function (user) {
                     if (user.Configuration.EnableNextEpisodeAutoPlay || nextMediaType !== MediaType.Video) {
                         self.nextTrack();
+
+                        if (newPlayer !== player) {
+                            Events.trigger(self, 'playbackstop', [{
+                                player: player,
+                                state: state,
+                                nextItem: nextItem,
+                                nextMediaType: nextItem.MediaType
+                            }]);
+                        }
                     }
                 });
             }
