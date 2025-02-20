f5732216434bbc2c53c355173b918ef7ec77c610
viown <48097677+viown@users.noreply.github.com>
2025-02-21 00:18:42 +0300

    Migrate scheduled tasks to React (#6506)
    * Migrate scheduled tasks to React
    
    * Adjust margins
    
    * Use localeCompare
    
    * Clean up imports
    
    * Use legacy apiclient from useApi
    
    * Fix import
    
    * Fix nested typography
    
    * Add polling fallback
    
    * Cleanup code
    
    * Rename to tasks
    
    * Rename to Component
    
    * Use constants for websocket events
    
    * Use memo to fix timestamp rerender on run

---
+++ src/apps/dashboard/features/scheduledtasks/components/Tasks.tsx
@@ -0,0 +1,29 @@
+import React, { FunctionComponent } from 'react';
+import type { TaskInfo } from '@jellyfin/sdk/lib/generated-client/models/task-info';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import Task from './Task';
+
+type TasksProps = {
+    category: string;
+    tasks: TaskInfo[];
+};
+
+const Tasks: FunctionComponent<TasksProps> = ({ category, tasks }: TasksProps) => {
+    return (
+        <Stack spacing={2}>
+            <Typography variant='h2'>{category}</Typography>
+            <List sx={{ bgcolor: 'background.paper' }}>
+                {tasks.map(task => {
+                    return <Task
+                        key={task.Id}
+                        task={task}
+                    />;
+                })}
+            </List>
+        </Stack>
+    );
+};
+
+export default Tasks;
