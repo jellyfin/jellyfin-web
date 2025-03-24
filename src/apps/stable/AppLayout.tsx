a0ddea0bfc67f1c8c75fd08ad33d1a822e788bc5
Bill Thornton <thornbill@users.noreply.github.com>
2025-03-24 13:11:25 -0400

    Fix custom css requiring page refresh

--- src/apps/stable/AppLayout.tsx
+++ src/apps/stable/AppLayout.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 import AppBody from 'components/AppBody';
-import ThemeCss from 'components/ThemeCss';
 import CustomCss from 'components/CustomCss';
+import ThemeCss from 'components/ThemeCss';
 
 export default function AppLayout() {
     return (
