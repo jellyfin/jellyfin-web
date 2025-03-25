a768ca3037f0fcd17d96977aafdd8035789f1f69
Bill Thornton <thornbill@users.noreply.github.com>
2025-03-25 12:21:57 -0400

    Update webpack config to build sass themes

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,23 @@
 const common = require('./webpack.common');
 const { merge } = require('webpack-merge');
 
+const THEMES = [
+    'appletv',
+    'blueradiance',
+    'dark',
+    'light',
+    'purplehaze',
+    'wmc'
+];
+
 module.exports = merge(common, {
     mode: 'production',
     entry: {
         'main.jellyfin': './index.jsx',
-        'serviceworker': './serviceworker.js'
+        'serviceworker': './serviceworker.js',
+        ...THEMES.reduce((acc, theme) => {
+            acc[`themes/${theme}`] = `./themes/${theme}/theme.scss`;
+            return acc;
+        }, {})
     }
 });
