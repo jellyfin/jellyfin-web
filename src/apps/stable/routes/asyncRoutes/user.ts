d4995834df2b77d2055a06588ae2beb00575cd8e
Bill Thornton <thornbill@users.noreply.github.com>
2025-04-01 15:10:30 -0400

    Move user settings menu to react (#6675)
    * Move user settings menu to react
    
    * Add default href value to LinkButton
    
    * Use loading component while data is pending

--- src/apps/stable/routes/asyncRoutes/user.ts
+++ src/apps/stable/routes/asyncRoutes/user.ts
@@ -1,6 +1,7 @@
 import { AsyncRoute } from '../../../../components/router/AsyncRoute';
 
 export const ASYNC_USER_ROUTES: AsyncRoute[] = [
+    { path: 'mypreferencesmenu', page: 'user/settings' },
     { path: 'quickconnect', page: 'quickConnect' },
     { path: 'search', page: 'search' },
     { path: 'userprofile', page: 'user/userprofile' }
