ShekiiNazarita/JellyFin

Add Series Timer Schedule Section

src/types/listOptions.ts
@@ -40,5 +40,6 @@ export interface ListOptions extends TextLineOpts {
     enableRatingButton?: boolean;
     smallIcon?: boolean;
     sortBy?: ItemSortBy;
-    queryKey?: string[]
+    queryKey?: string[];
+    runtime?: boolean
 }

jellyfin/jellyfin-web

src/types/listOptions.ts
@@ -41,5 +41,6 @@ export interface ListOptions extends TextLineOpts {
     enableRatingButton?: boolean;
     smallIcon?: boolean;
     sortBy?: ItemSortBy;
-    queryKey?: string[]
+    queryKey?: string[];
+    showRuntime?: boolean
 }