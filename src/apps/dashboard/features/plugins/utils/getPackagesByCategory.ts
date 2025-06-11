92cdfcc1ea9bbf6ad86184bcfbad06eb384837fc
viown <48097677+viown@users.noreply.github.com>
2025-06-11 16:53:51 +0300

    Migrate plugins catalog to React

---
+++ src/apps/dashboard/features/plugins/utils/getPackagesByCategory.ts
@@ -0,0 +1,15 @@
+import type { PackageInfo } from '@jellyfin/sdk/lib/generated-client/models/package-info';
+
+const getPackagesByCategory = (packages: PackageInfo[] | undefined, category: string) => {
+    if (!packages) return [];
+
+    return packages.filter(pkg => pkg.category == category).sort((a, b) => {
+        if (a.name && b.name) {
+            return a.name?.localeCompare(b.name);
+        } else {
+            return 0;
+        }
+    });
+};
+
+export default getPackagesByCategory;
