19162e00b1d6aefb6d52e58697fed30dab0dc9c5
Bill Thornton <thornbill@users.noreply.github.com>
2025-06-27 11:28:54 -0400

    Cleanup package filter
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/apps/dashboard/features/plugins/utils/getPackagesByCategory.ts
+++ src/apps/dashboard/features/plugins/utils/getPackagesByCategory.ts
@@ -3,13 +3,15 @@ import type { PackageInfo } from '@jellyfin/sdk/lib/generated-client/models/pack
 const getPackagesByCategory = (packages: PackageInfo[] | undefined, category: string) => {
     if (!packages) return [];
 
-    return packages.filter(pkg => pkg.category == category).sort((a, b) => {
-        if (a.name && b.name) {
-            return a.name?.localeCompare(b.name);
-        } else {
-            return 0;
-        }
-    });
+    return packages
+        .filter(pkg => pkg.category === category)
+        .sort((a, b) => {
+            if (a.name && b.name) {
+                return a.name.localeCompare(b.name);
+            } else {
+                return 0;
+            }
+        });
 };
 
 export default getPackagesByCategory;
