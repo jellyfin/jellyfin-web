c0981ebb8383632ee56215c077c3d2f2aa7634af
Alex Collado <57129654+a-collado@users.noreply.github.com>
2025-08-12 20:27:58 +0000

    Use culture name instead of 3 letter ISO language name

--- src/components/settingshelper.js
+++ src/components/settingshelper.js
@@ -11,7 +11,7 @@ export function populateLanguages(select, languages) {
     html += "<option value=''>" + globalize.translate('AnyLanguage') + '</option>';
     for (let i = 0, length = languages.length; i < length; i++) {
         const culture = languages[i];
-        html += "<option value='" + culture.ThreeLetterISOLanguageName + "'>" + culture.DisplayName + '</option>';
+        html += "<option value='" + culture.Name + "'>" + culture.DisplayName + '</option>';
     }
 
     select.innerHTML = html;
