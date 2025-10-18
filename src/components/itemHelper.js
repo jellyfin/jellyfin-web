3cb37f1d881722e589c91105beefffe453229e4a
Bill Thornton <thornbill@users.noreply.github.com>
2025-10-18 02:41:56 -0400

    Use Array.isArray for array type check

--- src/components/itemHelper.js
+++ src/components/itemHelper.js
@@ -52,10 +52,12 @@ export function getDisplayName(item, options = {}) {
         }
     }
 
-    if (item instanceof Array && item.length > 1) {
-        return item.map(i => getDisplayName(i, options)).join(' / ');
-    } else if (item.length === 1) {
-        return item[0].Name;
+    if (Array.isArray(item) {
+        if (item.length > 1) {
+            return item.map(i => getDisplayName(i, options)).join(' / ');
+        } else if (item.length === 1) {
+            return item[0].Name;
+        }
     }
 
     return name;
