3392b4a0b51828c3509e23d324993a98fb8f5220
viown <48097677+viown@users.noreply.github.com>
2025-10-28 08:47:53 +0300

    Add watch feature to log viewer

--- src/apps/dashboard/routes/logs/file.tsx
+++ src/apps/dashboard/routes/logs/file.tsx
@@ -1,6 +1,6 @@
 import Loading from 'components/loading/LoadingComponent';
 import Page from 'components/Page';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useServerLog } from 'apps/dashboard/features/logs/api/useServerLog';
 import Alert from '@mui/material/Alert';
@@ -12,6 +12,8 @@ import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import ContentCopy from '@mui/icons-material/ContentCopy';
 import FileDownload from '@mui/icons-material/FileDownload';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import globalize from 'lib/globalize';
 import { copy } from 'scripts/clipboard';
 import Toast from 'apps/dashboard/components/Toast';
@@ -24,13 +26,32 @@ export const Component = () => {
         data: log,
         refetch
     } = useServerLog(fileName ?? '');
+    const contentPrimaryRef = useRef<HTMLDivElement | null>(null);
     const [ isCopiedToastOpen, setIsCopiedToastOpen ] = useState(false);
+    const [ isWatchModeEnabled, setIsWatchModeEnabled ] = useState(false);
 
     const retry = useCallback(() => refetch(), [refetch]);
 
     const handleToastClose = useCallback(() => {
         setIsCopiedToastOpen(false);
     }, []);
+
+    const scrollToBottom = useCallback(() => {
+        const bottomHeight = contentPrimaryRef.current?.scrollHeight;
+
+        if (bottomHeight) {
+            window.scrollTo(0, bottomHeight);
+        }
+    }, []);
+
+    const toggleWatchMode = useCallback(() => {
+        const newWatchMode = !isWatchModeEnabled;
+        setIsWatchModeEnabled(newWatchMode);
+
+        if (newWatchMode) {
+            scrollToBottom();
+        }
+    }, [isWatchModeEnabled, scrollToBottom]);
 
     const copyToClipboard = useCallback(async () => {
         if (log) {
@@ -51,6 +72,24 @@ export const Component = () => {
         }
     }, [log, fileName]);
 
+    useEffect(() => {
+        const watchInterval = setInterval(() => {
+            if (isWatchModeEnabled && contentPrimaryRef.current) {
+                const wasAtBottom = window.innerHeight + Math.round(window.scrollY) >= contentPrimaryRef.current.offsetHeight;
+
+                void refetch().then(() => {
+                    if (wasAtBottom) {
+                        setTimeout(scrollToBottom, 0);
+                    }
+                });
+            }
+        }, 2000);
+
+        return () => {
+            clearInterval(watchInterval);
+        };
+    }, [ isWatchModeEnabled, refetch, scrollToBottom ]);
+
     return (
         <Page
             id='logPage'
@@ -62,7 +101,11 @@ export const Component = () => {
                 onClose={handleToastClose}
                 message={globalize.translate('CopyLogSuccess')}
             />
-            <Container className='content-primary' maxWidth={false}>
+            <Container
+                className='content-primary'
+                maxWidth={false}
+                ref={contentPrimaryRef}
+            >
                 <Box>
                     <Typography variant='h1'>{fileName}</Typography>
 
@@ -101,6 +144,12 @@ export const Component = () => {
                                     onClick={downloadFile}
                                 >
                                     {globalize.translate('Download')}
+                                </Button>
+                                <Button
+                                    startIcon={isWatchModeEnabled ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                                    onClick={toggleWatchMode}
+                                >
+                                    {isWatchModeEnabled ? globalize.translate('Unwatch') : globalize.translate('Watch')}
                                 </Button>
                             </ButtonGroup>
 
