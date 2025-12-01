b78730b194c5842782f7aa7bcb541fe65dfc8e18
Andreas B <6439218+YouKnowBlom@users.noreply.github.com>
2025-12-01 09:22:31 +0100

    Add ability to change inactive session threshold setting through frontend

--- src/apps/dashboard/routes/playback/streaming.tsx
+++ src/apps/dashboard/routes/playback/streaming.tsx
@@ -25,6 +25,10 @@ export const action = async ({ request }: ActionFunctionArgs) => {
     const bitrateLimit = formData.get('StreamingBitrateLimit')?.toString();
     config.RemoteClientBitrateLimit = Math.trunc(1e6 * parseFloat(bitrateLimit || '0'));
 
+    const inactiveSessionThreshold = formData.get('InactiveSessionThreshold')?.toString();
+
+    if (inactiveSessionThreshold) config.InactiveSessionThreshold = parseInt(inactiveSessionThreshold, 10);
+
     await getConfigurationApi(api)
         .updateConfiguration({ serverConfiguration: config });
 
@@ -78,6 +82,20 @@ export const Component = () => {
                                 htmlInput: {
                                     min: 0,
                                     step: 0.25
+                                }
+                            }}
+                        />
+                        <TextField
+                            label={globalize.translate('LabelInactiveSessionThreshold')}
+                            name='InactiveSessionThreshold'
+                            type='number'
+                            defaultValue={defaultConfiguration?.InactiveSessionThreshold}
+                            helperText={globalize.translate('LabelInactiveSessionThresholdHelp')}
+                            slotProps={{
+                                htmlInput: {
+                                    min: 0,
+                                    max: 60 * 24,
+                                    required: true
                                 }
                             }}
                         />
