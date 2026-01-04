dce40880f2c29859bfbbc9d2f42c87178c92c646
Natalí Paura <30585029+natilou@users.noreply.github.com>
2026-01-04 11:57:36 -0300

    blur title and description in episode details and More from Season section

--- src/controllers/itemDetails/index.js
+++ src/controllers/itemDetails/index.js
@@ -482,7 +482,11 @@ function renderName(item, container, context) {
 
     if (html && !parentNameLast) {
         if (tvSeasonHtml) {
-            html += '<h3 class="itemName infoText subtitle focuscontainer-x"><bdi>' + tvSeasonHtml + ' - ' + name + '</bdi></h3>';
+            html += '<h3 class="itemName infoText subtitle focuscontainer-x';
+            if (!item.UserData.Played && userSettings.enableBlurUnplayedTitle()) {
+                html += ' listItemBodyText-blurred';
+            }
+            html += '"><bdi>' + tvSeasonHtml + ' - ' + name + '</bdi></h3>';
         } else {
             html += '<h3 class="itemName infoText subtitle"><bdi>' + name + '</bdi></h3>';
         }
@@ -927,6 +931,10 @@ function renderOverview(page, item) {
                 for (const anchor of overviewElemnt.querySelectorAll('a')) {
                     anchor.setAttribute('target', '_blank');
                 }
+
+                if (item.Type === 'Episode' && !item.UserData.Played && userSettings.enableBlurUnplayedDescription()) {
+                    overviewElemnt.classList.add('listItemBodyText-blurred');
+                }
             }
         } else {
             for (const overviewElemnt of overviewElements) {
@@ -1091,7 +1099,8 @@ function renderMoreFromSeason(view, item, apiClient) {
                 overlayText: false,
                 centerText: true,
                 includeParentInfoInTitle: false,
-                allowBottomPadding: false
+                allowBottomPadding: false,
+                enableBlurUnplayedTitle: userSettings.enableBlurUnplayedTitle()
             });
             const card = itemsContainer.querySelector('.card[data-id="' + item.Id + '"]');
 
@@ -1328,9 +1337,7 @@ function renderChildren(page, item) {
                 action: 'playallfromhere',
                 image: false,
                 artist: showArtist,
-                containerAlbumArtists: item.AlbumArtists,
-                enableBlurUnplayedTitle: userSettings.enableBlurUnplayedTitle(),
-                enableBlurUnplayedDescription: userSettings.enableBlurUnplayedDescription()
+                containerAlbumArtists: item.AlbumArtists
             });
             isList = true;
         } else if (item.Type == 'Series') {
@@ -1474,6 +1481,8 @@ function renderProgramsForChannel(page, result) {
     let html = '';
     let currentItems = [];
     let currentStartDate = null;
+    const enableBlurUnplayedTitle = userSettings.enableBlurUnplayedTitle();
+    const enableBlurUnplayedDescription = userSettings.enableBlurUnplayedDescription();
 
     for (let i = 0, length = result.Items.length; i < length; i++) {
         const item = result.Items[i];
@@ -1495,8 +1504,8 @@ function renderProgramsForChannel(page, result) {
                     showProgramTime: true,
                     mediaInfo: false,
                     parentTitleWithTitle: true,
-                    enableBlurUnplayedTitle: userSettings.enableBlurUnplayedTitle(),
-                    enableBlurUnplayedDescription: userSettings.enableBlurUnplayedDescription()
+                    enableBlurUnplayedTitle: enableBlurUnplayedTitle,
+                    enableBlurUnplayedDescription: enableBlurUnplayedDescription
                 }) + '</div></div>';
             }
 
@@ -1522,8 +1531,8 @@ function renderProgramsForChannel(page, result) {
             showProgramTime: true,
             mediaInfo: false,
             parentTitleWithTitle: true,
-            enableBlurUnplayedTitle: userSettings.enableBlurUnplayedTitle(),
-            enableBlurUnplayedDescription: userSettings.enableBlurUnplayedDescription()
+            enableBlurUnplayedTitle: enableBlurUnplayedTitle,
+            enableBlurUnplayedDescription: enableBlurUnplayedDescription
         }) + '</div></div>';
     }
 
