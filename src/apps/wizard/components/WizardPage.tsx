bcd86b8ade9e41916e3c7439ec644cd59432b032
viown <48097677+viown@users.noreply.github.com>
2026-01-19 00:08:51 +0300

    Use form event handlers

--- src/apps/wizard/components/WizardPage.tsx
+++ src/apps/wizard/components/WizardPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Container from '@mui/material/Container';
 import Page, { type PageProps } from 'components/Page';
 import Stack from '@mui/material/Stack';
@@ -7,6 +7,7 @@ import globalize from 'lib/globalize';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import CheckIcon from '@mui/icons-material/Check';
+import { Form } from 'react-router-dom';
 
 interface WizardPageProps extends PageProps {
     onNext?: () => void;
@@ -15,48 +16,60 @@ interface WizardPageProps extends PageProps {
 }
 
 const WizardPage = ({ children, onNext, onPrevious, onFinish, ...pageProps }: WizardPageProps) => {
+    const onSubmit = useCallback((e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (onNext) {
+            onNext();
+        } else if (onFinish) {
+            onFinish();
+        }
+    }, [onNext, onFinish]);
+
     return (
         <Page
             className='mainAnimatedPage type-interior wizardPage'
             {...pageProps}
         >
             <Container className='padded-top' maxWidth='md'>
-                {children}
+                <Form method='POST' onSubmit={onSubmit}>
+                    {children}
 
-                <Stack
-                    mt={6}
-                    justifyContent={'flex-end'}
-                    direction='row'
-                    gap={1}
-                >
-                    {onPrevious ? (
-                        <Button
-                            startIcon={<ArrowBackIcon />}
-                            variant='outlined'
-                            onClick={onPrevious}
-                        >
-                            {globalize.translate('Previous')}
-                        </Button>
-                    ) : null}
+                    <Stack
+                        mt={6}
+                        justifyContent={'flex-end'}
+                        direction='row'
+                        gap={1}
+                    >
+                        {onPrevious ? (
+                            <Button
+                                startIcon={<ArrowBackIcon />}
+                                variant='outlined'
+                                onClick={onPrevious}
+                            >
+                                {globalize.translate('Previous')}
+                            </Button>
+                        ) : null}
 
-                    {onNext ? (
-                        <Button
-                            endIcon={<ArrowForwardIcon />}
-                            onClick={onNext}
-                        >
-                            {globalize.translate('Next')}
-                        </Button>
-                    ) : null}
+                        {onNext ? (
+                            <Button
+                                endIcon={<ArrowForwardIcon />}
+                                type='submit'
+                            >
+                                {globalize.translate('Next')}
+                            </Button>
+                        ) : null}
 
-                    {onFinish ? (
-                        <Button
-                            endIcon={<CheckIcon />}
-                            onClick={onFinish}
-                        >
-                            {globalize.translate('LabelFinish')}
-                        </Button>
-                    ) : null}
-                </Stack>
+                        {onFinish ? (
+                            <Button
+                                endIcon={<CheckIcon />}
+                                type='submit'
+                            >
+                                {globalize.translate('LabelFinish')}
+                            </Button>
+                        ) : null}
+                    </Stack>
+                </Form>
             </Container>
         </Page>
     );
