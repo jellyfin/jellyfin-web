6bdc794873c57064bc42efa3f0a61381446493c8
Bill Thornton <thornbill@users.noreply.github.com>
2026-02-26 17:29:25 -0500

    Add query caching to indexeddb

--- src/hooks/useUserViews.ts
+++ src/hooks/useUserViews.ts
@@ -24,9 +24,6 @@ export const getUserViewsQuery = (
 ) => queryOptions({
     queryKey: [ 'User', userId, 'Views', params ],
     queryFn: ({ signal }) => fetchUserViews(api!, userId!, params, { signal }),
-    // On initial page load we request user views 3x. Setting a 1 second stale time
-    // allows a single request to be made to resolve all 3.
-    staleTime: 1000, // 1 second
     enabled: !!api && !!userId
 });
 
