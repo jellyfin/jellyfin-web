3e80675319407a15acbdced9cdb84d1676ccb317
JPVenson <github@jpb.email>
2026-03-13 11:01:25 +0000

    change window usage to globalThis

--- src/scripts/browserDeviceProfile.js
+++ src/scripts/browserDeviceProfile.js
@@ -412,7 +412,7 @@ function getGlobalMaxVideoBitrate() {
     if (browser.ps4) {
         bitrate = 8000000;
     } else if (browser.xboxOne) {
-        bitrate = window.xboxBitrateLimit ?? null;
+        bitrate = globalThis.xboxBitrateLimit ?? null;
     } else if (browser.tizen && isTizenFhd) {
         bitrate = 20000000;
     }
