b44c2079a08c36209a91fceb6a73fe3ad396158f
The-Coomer <80853142+The-Coomer@users.noreply.github.com>
2026-03-21 17:15:43 +0000

    Return maxMaxBufferLength to default value

--- src/plugins/htmlVideoPlayer/plugin.js
+++ src/plugins/htmlVideoPlayer/plugin.js
@@ -456,7 +456,6 @@ export class HtmlVideoPlayer {
                     startPosition: options.playerStartPositionTicks / 10000000,
                     manifestLoadingTimeOut: 20000,
                     maxBufferLength: maxBufferLength,
-                    maxMaxBufferLength: maxBufferLength,
                     videoPreference: { preferHDR: true },
                     xhrSetup(xhr) {
                         xhr.withCredentials = includeCorsCredentials;
