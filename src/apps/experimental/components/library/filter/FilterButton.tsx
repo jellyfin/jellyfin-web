bca800e922d187ac61c9ee9f4f19dad3e76b3b04
Shadowghost <Ghost_of_Stone@web.de>
2026-04-11 17:52:10 +0200

    Make Linter happy

--- src/apps/experimental/components/library/filter/FilterButton.tsx
+++ src/apps/experimental/components/library/filter/FilterButton.tsx
@@ -161,13 +161,6 @@ const FilterButton: FC<FilterButtonProps> = ({
         );
     };
 
-    const isFiltersVideoTypesEnabled = () => {
-        return (
-            viewType === LibraryTab.Movies
-            || viewType === LibraryTab.Series
-            || viewType === LibraryTab.Episodes
-        );
-    };
 
     const isFiltersSeriesStatusEnabled = () => {
         return viewType === LibraryTab.Series;
@@ -302,7 +295,7 @@ const FilterButton: FC<FilterButtonProps> = ({
                     </Accordion>
                 )}
 
-                {isFiltersVideoTypesEnabled() && (
+                {isFiltersFeaturesEnabled() && (
                     <Accordion
                         expanded={expanded === 'filtersVideoTypes'}
                         onChange={handleChange('filtersVideoTypes')}
