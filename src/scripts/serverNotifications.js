7e2077ad4ef6c8eac436c0a046e0109b0037834a
Violet Caulfield <42452695+anultravioletaurora@users.noreply.github.com>
2026-06-17 14:12:12 -0500

    revert serverNotification.js to original state

--- src/scripts/serverNotifications.js
+++ src/scripts/serverNotifications.js
@@ -192,17 +192,17 @@ function onPlaystate({ Data }) {
  */
 function subscribeToApiClient(apiClient) {
     const subscriptions = [
-        apiClient.subscribe?.([OutboundWebSocketMessageType.Play], (msg) => onPlay(msg, apiClient)),
-        apiClient.subscribe?.([OutboundWebSocketMessageType.Playstate], (msg) => onPlaystate(msg)),
-        apiClient.subscribe?.([OutboundWebSocketMessageType.GeneralCommand], ({ Data }) => processGeneralCommand(Data, apiClient)),
-        apiClient.subscribe?.([OutboundWebSocketMessageType.SyncPlayCommand], ({ Data }) => {
+        apiClient.subscribe([OutboundWebSocketMessageType.Play], (msg) => onPlay(msg, apiClient)),
+        apiClient.subscribe([OutboundWebSocketMessageType.Playstate], (msg) => onPlaystate(msg)),
+        apiClient.subscribe([OutboundWebSocketMessageType.GeneralCommand], ({ Data }) => processGeneralCommand(Data, apiClient)),
+        apiClient.subscribe([OutboundWebSocketMessageType.SyncPlayCommand], ({ Data }) => {
             pluginManager.firstOfType(PluginType.SyncPlay)?.instance.Manager.processCommand(Data, apiClient);
         }),
-        apiClient.subscribe?.([OutboundWebSocketMessageType.SyncPlayGroupUpdate], ({ Data }) => {
+        apiClient.subscribe([OutboundWebSocketMessageType.SyncPlayGroupUpdate], ({ Data }) => {
             pluginManager.firstOfType(PluginType.SyncPlay)?.instance.Manager.processGroupUpdate(Data, apiClient);
             Events.trigger(serverNotifications, OutboundWebSocketMessageType.SyncPlayGroupUpdate, [apiClient, Data]);
         })
-    ].filter(Boolean);
+    ];
 
     return () => subscriptions.get(apiClient.serverId()).forEach((unsub) => {
         unsub();
@@ -210,18 +210,11 @@ function subscribeToApiClient(apiClient) {
 }
 
 export function initializeServerConnections() {
-    let unsub;
+    ServerConnections.getApiClients().forEach(subscribeToApiClient);
 
-    const onApiClientCreated = (e, newApiClient) => {
-        unsub = subscribeToApiClient(newApiClient);
-    };
-
-    const onLocalUserSignedOut = () => {
-        unsub();
-    };
-
-    Events.on(ServerConnections, 'apiclientcreated', onApiClientCreated);
-    Events.on(ServerConnections, 'localusersignedout', () => onLocalUserSignedOut);
+    Events.on(ServerConnections, 'apiclientcreated', function (e, newApiClient) {
+        subscribeToApiClient(newApiClient);
+    });
 }
 
 window.ServerNotifications = serverNotifications;
