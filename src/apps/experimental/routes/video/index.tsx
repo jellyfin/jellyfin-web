f03387770cfbcb6204d5d0909a4ddb9b2a727b33
Shindou Jeikobu <shindouj@users.noreply.github.com>
2026-06-30 16:33:45 +0200

    Restore OSD header's background in experimental mode (#8081)
    * Restored gradient/blur in the background (Fixes #8000)
    
    * Fixed stylelint issues
    
    * Removed override, replaced with sx style

--- src/apps/experimental/routes/video/index.tsx
+++ src/apps/experimental/routes/video/index.tsx
@@ -48,13 +48,17 @@ const VideoPage: FC = () => {
                 in={isVisible}
                 easing='fade-out'
             >
-                <Box sx={{
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                    right: 0,
-                    color: 'white'
-                }}>
+                <Box
+                    className='skinHeader skinHeader-withBackground skinHeader-blurred osdHeader'
+                    sx={{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                        color: 'white',
+                        pointerEvents: 'unset !important'
+                    }}
+                >
                     <AppToolbar
                         isDrawerAvailable={false}
                         isDrawerOpen={false}
