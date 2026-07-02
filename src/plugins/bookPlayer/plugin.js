1beabcd07c59493e188bcd09e5f7431077cb377f
dkanada <dkanada@users.noreply.github.com>
2026-07-02 10:31:19 +0900

    improve font size selection in epub files

--- src/plugins/bookPlayer/plugin.js
+++ src/plugins/bookPlayer/plugin.js
@@ -24,9 +24,9 @@ import html from './template.html';
 import './style.scss';
 
 const THEMES = {
-    'dark': { 'body': { 'color': '#d8dadc', 'background': '#000', 'font-size': 'medium' } },
-    'sepia': { 'body': { 'color': '#d8a262', 'background': '#000', 'font-size': 'medium' } },
-    'light': { 'body': { 'color': '#000', 'background': '#fff', 'font-size': 'medium' } }
+    'dark': { 'body': { 'color': '#d8dadc', 'background': '#000' } },
+    'sepia': { 'body': { 'color': '#d8a262', 'background': '#000' } },
+    'light': { 'body': { 'color': '#000', 'background': '#fff' } }
 };
 const THEME_ORDER = ['dark', 'sepia', 'light'];
 const FONT_SIZES = ['x-small', 'small', 'medium', 'large', 'x-large'];
@@ -43,10 +43,10 @@ export class BookPlayer {
         } else {
             this.theme = 'light';
         }
-        this.fontSize = 'medium';
         this.onDialogClosed = this.onDialogClosed.bind(this);
         this.openTableOfContents = this.openTableOfContents.bind(this);
         this.rotateTheme = this.rotateTheme.bind(this);
+        this.setTheme = this.setTheme.bind(this);
         this.increaseFontSize = this.increaseFontSize.bind(this);
         this.decreaseFontSize = this.decreaseFontSize.bind(this);
         this.previous = this.previous.bind(this);
@@ -193,7 +193,7 @@ export class BookPlayer {
         this.rendition?.on('keydown', this.onWindowKeyDown);
 
         if (browser.safari) {
-            const player = document.querySelector('.bookOsd');
+            const player = document.querySelector('#bookPlayerContainer');
             this.addSwipeGestures(player);
         } else {
             this.rendition?.on('rendered', (e, i) => this.addSwipeGestures(i.document.documentElement));
@@ -218,6 +218,28 @@ export class BookPlayer {
         }
     }
 
+    setTheme(theme, fontSize) {
+        if (!this.loaded) return;
+
+        this.theme = theme;
+        this.fontSize = fontSize;
+
+        const defaultTheme = {
+            ...THEMES[theme],
+            'a': { 'color': `${getComputedStyle(document.documentElement).getPropertyValue('--jf-palette-primary-main')}` }
+        };
+
+        // force this style on all text only when the user has opted to change the default
+        // many epub files will apply font size to individual elements and thus ignore a value on document body
+        // TODO find accurate method to differentiate between headers and paragraphs
+        if (fontSize !== undefined) {
+            defaultTheme['a, p, div, li, span'] = { 'font-size': `${fontSize} !important` };
+        }
+
+        this.rendition.themes.register('default', defaultTheme);
+        this.rendition.themes.update('default');
+    }
+
     toggleFullscreen() {
         const player = document.querySelector('#bookPlayerContainer');
 
@@ -236,28 +258,19 @@ export class BookPlayer {
     }
 
     rotateTheme() {
-        if (this.loaded) {
-            const newTheme = THEME_ORDER[(THEME_ORDER.indexOf(this.theme) + 1) % THEME_ORDER.length];
-            this.rendition.themes.register('default', THEMES[newTheme]);
-            this.rendition.themes.update('default');
-            this.theme = newTheme;
-        }
+        this.setTheme(THEME_ORDER[(THEME_ORDER.indexOf(this.theme) + 1) % THEME_ORDER.length], this.fontSize);
     }
 
     increaseFontSize() {
-        if (this.loaded && this.fontSize !== FONT_SIZES[FONT_SIZES.length - 1]) {
-            const newFontSize = FONT_SIZES[(FONT_SIZES.indexOf(this.fontSize) + 1)];
-            this.rendition.themes.fontSize(newFontSize);
-            this.fontSize = newFontSize;
-        }
+        if (this.fontSize === FONT_SIZES[FONT_SIZES.length - 1]) return;
+
+        this.setTheme(this.theme, FONT_SIZES[(FONT_SIZES.indexOf(this.fontSize ?? 'medium') + 1)]);
     }
 
     decreaseFontSize() {
-        if (this.loaded && this.fontSize !== FONT_SIZES[0]) {
-            const newFontSize = FONT_SIZES[(FONT_SIZES.indexOf(this.fontSize) - 1)];
-            this.rendition.themes.fontSize(newFontSize);
-            this.fontSize = newFontSize;
-        }
+        if (this.fontSize === FONT_SIZES[0]) return;
+
+        this.setTheme(this.theme, FONT_SIZES[(FONT_SIZES.indexOf(this.fontSize ?? 'medium') - 1)]);
     }
 
     previous(e) {
@@ -341,9 +354,6 @@ export class BookPlayer {
                 this.currentSrc = downloadHref;
                 this.rendition = rendition;
 
-                rendition.themes.register('default', THEMES[this.theme]);
-                rendition.themes.select('default');
-
                 return rendition.display().then(() => {
                     const epubElem = document.querySelector('.epub-container');
                     epubElem.style.opacity = '0';
@@ -366,6 +376,7 @@ export class BookPlayer {
                             Events.trigger(this, 'pause');
                         });
 
+                        this.setTheme(this.theme, this.fontSize);
                         loading.hide();
                         return resolve();
                     });
