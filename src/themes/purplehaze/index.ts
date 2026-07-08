5860b44129dddf0e053b50a8c922d788114bbbe9
Bill Thornton <thornbill@users.noreply.github.com>
2026-07-08 17:24:05 -0400

    Refactor purple haze to use base theme

--- src/themes/purplehaze/index.ts
+++ src/themes/purplehaze/index.ts
@@ -4,7 +4,8 @@ import { buildCustomColorScheme } from 'themes/utils';
 const theme = buildCustomColorScheme({
     palette: {
         background: {
-            paper: '#000420'
+            default: '#230c33',
+            paper: '#230c33'
         },
         primary: {
             main: '#48c3c8'
@@ -12,8 +13,32 @@ const theme = buildCustomColorScheme({
         secondary: {
             main: '#ff77f1'
         },
+        text: {
+            primary: '#f8f8fe',
+            secondary: 'rgba(248, 248, 254, 0.973)'
+        },
+        action: {
+            focus: 'rgba(0, 0, 0, 0.3)',
+            hover: '#12122f'
+        },
+        divider: 'rgba(255, 255, 255, 0.14)',
+        Alert: {
+            infoFilledBg: '#dbe6ff',
+            infoFilledColor: '#0e0f2d'
+        },
         AppBar: {
-            defaultBg: '#000420'
+            defaultBg: '#230c33'
+        },
+        Button: {
+            inheritContainedBg: 'rgba(0, 0, 0, 0.5)',
+            inheritContainedHoverBg: 'rgb(12, 232, 214)'
+        },
+        FilledInput: {
+            bg: 'rgba(0, 0, 0, 0.5)'
+        },
+        SnackbarContent: {
+            bg: '#303030',
+            color: 'rgba(255, 255, 255, 0.87)'
         }
     }
 });
