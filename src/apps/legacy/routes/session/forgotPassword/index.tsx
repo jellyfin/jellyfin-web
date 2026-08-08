22f93ce4630e0d74d57143bbd8ec3da3feb7edb5
viown <48097677+viown@users.noreply.github.com>
2026-08-08 19:42:13 +0300

    Use shell.openUrl

--- src/apps/legacy/routes/session/forgotPassword/index.tsx
+++ src/apps/legacy/routes/session/forgotPassword/index.tsx
@@ -11,6 +11,7 @@ import globalize from 'lib/globalize';
 import ServerConnections from 'lib/jellyfin-apiclient/ServerConnections';
 import { getAuthenticationApi } from 'utils/sdk/authentication-api';
 import { isValidUrl } from 'utils/url';
+import shell from 'scripts/shell';
 
 export const ForgotPasswordPage = () => {
     const navigate = useNavigate();
@@ -44,7 +45,7 @@ export const ForgotPasswordPage = () => {
                 case ForgotPasswordAction.PinCode:
                     if (result.PinFile && isValidUrl(result.PinFile)) {
                         msg += globalize.translate('MessageForgotPasswordRedirect');
-                        window.open(result.PinFile, '_blank', 'noopener,noreferrer');
+                        shell.openUrl(result.PinFile);
                         callback = () => navigate('/login');
                     } else {
                         msg = globalize.translate('MessageForgotPasswordFileCreated');
